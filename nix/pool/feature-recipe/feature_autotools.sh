2ab431733c58b1b89ba6bac2aad678905ede0f3c
StudioEtrange <nomorgan@gmail.com>
2015-02-06 02:21:36 +0100

    linux : fix feature autotools

--- nix/pool/feature-recipe/feature_autotools.sh
+++ nix/pool/feature-recipe/feature_autotools.sh
@@ -40,32 +40,41 @@ function __feature_autotools() {
 function __install_autotools_pack() {
 	# order is important
 	# see http://petio.org/tools.html
-	__install_m4_1_4_17
+	__install_autotools_m4_1_4_17
 	__init_feature autotools m4_1_4_17
-	__install_autoconf_2_69
+	__install_autotools_autoconf_2_69
 	__init_feature autotools autoconf_2_69
-	__install_automake_1_14
+	__install_autotools_automake_1_14
 	__init_feature autotools automake_1_14
-	__install_libtool_2_4_2
+	__install_autotools_libtool_2_4_2
 	__init_feature autotools libtool_2_4_2
 }
 function __feature_autotools_pack() {
 	TEST_FEATURE=0
 	FEATURE_PATH=
 	FEATURE_VER=
+
+	local _flag=1
+
 	__feature_autotools_m4_1_4_17
-	[ "$TEST_FEATURE" == "0" ] && return
+	[ "$TEST_FEATURE" == "0" ] && _flag=0
 	__feature_autotools_autoconf_2_69
-	[ "$TEST_FEATURE" == "0" ] && return
+	[ "$TEST_FEATURE" == "0" ] && _flag=0
 	__feature_autotools_automake_1_14
-	[ "$TEST_FEATURE" == "0" ] && return
+	[ "$TEST_FEATURE" == "0" ] && _flag=0
 	__feature_autotools_libtool_2_4_2
-	[ "$TEST_FEATURE" == "0" ] && return
-	if [ "$TEST_FEATURE" == "1" ]; then
+	[ "$TEST_FEATURE" == "0" ] && _flag=0
+
+	if [ "$_flag" == "1" ]; then
+		TEST_FEATURE=1
 		[ "$VERBOSE_MODE" == "0" ] || echo " ** EXTRA FEATURE Detected : autotools in $FEATURE_RESULT_ROOT"
 		FEATURE_ROOT="$STELLA_APP_FEATURE_ROOT/autotools"
 		FEATURE_PATH="$FEATURE_ROOT/bin"
 		FEATURE_VER=pack
+	else
+		TEST_FEATURE=0
+		FEATURE_PATH=
+		FEATURE_VER=
 	fi
 }
 
@@ -103,11 +112,12 @@ function __feature_autotools_autoconf_2_69() {
 	FEATURE_RESULT_PATH="$FEATURE_RESULT_ROOT/bin"
 	FEATURE_RESULT_VER="2_69"
 	__feature_autotools_internal autoconf
+	[ "$TEST_FEATURE" == "1" ] && $FEATURE_RESULT_ROOT/bin/autoconf --version | sed -ne "1,1p"
 	FEATURE_RESULT_PATH=
 	FEATURE_RESULT_ROOT=
 	FEATURE_RESULT_VER=
 
-	[ "$TEST_FEATURE" == "1" ] && $FEATURE_RESULT_ROOT/bin/autoconf --version | sed -ne "1,1p"
+	
 }
 
 
@@ -138,11 +148,11 @@ function __feature_autotools_automake_1_14() {
 	FEATURE_RESULT_PATH="$FEATURE_RESULT_ROOT/bin"
 	FEATURE_RESULT_VER="1_14"
 	__feature_autotools_internal automake
+	[ "$TEST_FEATURE" == "1" ] && $FEATURE_RESULT_ROOT/bin/automake --version | sed -ne "1,1p"
 	FEATURE_RESULT_PATH=
 	FEATURE_RESULT_ROOT=
 	FEATURE_RESULT_VER=
 
-	[ "$TEST_FEATURE" == "1" ] && $FEATURE_RESULT_ROOT/bin/automake --version | sed -ne "1,1p"
 }
 
 
@@ -159,7 +169,7 @@ function __install_autotools_libtool_2_4_2() {
 	AUTO_INSTALL_FLAG_PREFIX=
 	AUTO_INSTALL_FLAG_POSTFIX=
 
-	feature_autotools_libtool_2_4_2
+	__feature_autotools_libtool_2_4_2
 	[ "$FORCE" ] && TEST_FEATURE=0
 	if [ "$TEST_FEATURE" == "0" ]; then
 		__auto_install "configure" "libtool" "$FILE_NAME" "$URL" "$SRC_DIR" "$BUILD_DIR" "$INSTALL_DIR" "STRIP"
@@ -173,11 +183,12 @@ function __feature_autotools_libtool_2_4_2() {
 	FEATURE_RESULT_PATH="$FEATURE_RESULT_ROOT/bin"
 	FEATURE_RESULT_VER="2_4_2"
 	__feature_autotools_internal libtool
+	[ "$TEST_FEATURE" == "1" ] && $FEATURE_RESULT_ROOT/bin/libtool --version | sed -ne "1,1p"
 	FEATURE_RESULT_PATH=
 	FEATURE_RESULT_ROOT=
 	FEATURE_RESULT_VER=
 
-	[ "$TEST_FEATURE" == "1" ] && $FEATURE_RESULT_ROOT/bin/libtool --version | sed -ne "1,1p"
+	
 }
 
 
@@ -208,11 +219,12 @@ function __feature_autotools_m4_1_4_17() {
 	FEATURE_RESULT_PATH="$FEATURE_RESULT_ROOT/bin"
 	FEATURE_RESULT_VER="1_4_17"
 	__feature_autotools_internal m4
+	[ "$TEST_FEATURE" == "1" ] && $FEATURE_RESULT_ROOT/bin/m4 --version | sed -ne "1,1p"
 	FEATURE_RESULT_PATH=
 	FEATURE_RESULT_ROOT=
 	FEATURE_RESULT_VER=
 
-	[ "$TEST_FEATURE" == "1" ] && $FEATURE_RESULT_ROOT/bin/m4 --version | sed -ne "1,1p"
+	
 }
 
 
