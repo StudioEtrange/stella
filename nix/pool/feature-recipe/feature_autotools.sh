cececf29549f0691dd4e01d772a6e7a8ec3ac354
StudioEtrange <nomorgan@gmail.com>
2015-02-06 01:59:22 +0100

    linux : fix feature autotools

--- nix/pool/feature-recipe/feature_autotools.sh
+++ nix/pool/feature-recipe/feature_autotools.sh
@@ -53,15 +53,20 @@ function __feature_autotools_pack() {
 	TEST_FEATURE=0
 	FEATURE_PATH=
 	FEATURE_VER=
-	if [ -f "$STELLA_APP_FEATURE_ROOT/autotools/bin/autoconf" ]; then
-		TEST_FEATURE=1
+	__feature_autotools_m4_1_4_17
+	[ "$TEST_FEATURE" == "0" ] && return
+	__feature_autotools_autoconf_2_69
+	[ "$TEST_FEATURE" == "0" ] && return
+	__feature_autotools_automake_1_14
+	[ "$TEST_FEATURE" == "0" ] && return
+	__feature_autotools_libtool_2_4_2
+	[ "$TEST_FEATURE" == "0" ] && return
+	if [ "$TEST_FEATURE" == "1" ]; then
 		[ "$VERBOSE_MODE" == "0" ] || echo " ** EXTRA FEATURE Detected : autotools in $FEATURE_RESULT_ROOT"
 		FEATURE_ROOT="$STELLA_APP_FEATURE_ROOT/autotools"
 		FEATURE_PATH="$FEATURE_ROOT/bin"
 		FEATURE_VER=pack
 	fi
-
-
 }
 
 
@@ -69,7 +74,7 @@ function __feature_autotools_pack() {
 
 # ---------------------------------------
 
-function __install_autoconf_2_69() {
+function __install_autotools_autoconf_2_69() {
 	URL=http://ftp.gnu.org/gnu/autoconf/autoconf-2.69.tar.gz
 	VER=2.69
 	FILE_NAME=autoconf-2.69.tar.gz
@@ -84,7 +89,7 @@ function __install_autoconf_2_69() {
 	AUTO_INSTALL_FLAG_PREFIX=
 	AUTO_INSTALL_FLAG_POSTFIX="--docdir=$INSTALL_DIR/share/doc/automake-1.14"
 
-	__feature_autoconf_2_69
+	__feature_autotools_autoconf_2_69
 	[ "$FORCE" ] && TEST_FEATURE=0
 	if [ "$TEST_FEATURE" == "0" ]; then
 		__auto_install "configure" "autoconf" "$FILE_NAME" "$URL" "$SRC_DIR" "$BUILD_DIR" "$INSTALL_DIR" "STRIP"
@@ -92,7 +97,7 @@ function __install_autoconf_2_69() {
 		echo " ** Already installed"
 	fi
 }
-function __feature_autoconf_2_69() {
+function __feature_autotools_autoconf_2_69() {
 	FEATURE_TEST="$STELLA_APP_FEATURE_ROOT/autotools/bin/autoconf"
 	FEATURE_RESULT_ROOT="$STELLA_APP_FEATURE_ROOT/autotools"
 	FEATURE_RESULT_PATH="$FEATURE_RESULT_ROOT/bin"
@@ -108,7 +113,7 @@ function __feature_autoconf_2_69() {
 
 
 
-function __install_automake_1_14() {
+function __install_autotools_automake_1_14() {
 	URL=http://ftp.gnu.org/gnu/automake/automake-1.14.tar.gz
 	VER=1.14
 	FILE_NAME=automake-1.14.tar.gz
@@ -119,7 +124,7 @@ function __install_automake_1_14() {
 	AUTO_INSTALL_FLAG_PREFIX=
 	AUTO_INSTALL_FLAG_POSTFIX="--docdir=$INSTALL_DIR/share/doc/automake-1.14"
 
-	__feature_automake_1_14
+	__feature_autotools_automake_1_14
 	[ "$FORCE" ] && TEST_FEATURE=0
 	if [ "$TEST_FEATURE" == "0" ]; then
 		__auto_install "configure" "automake" "$FILE_NAME" "$URL" "$SRC_DIR" "$BUILD_DIR" "$INSTALL_DIR" "STRIP"
@@ -127,7 +132,7 @@ function __install_automake_1_14() {
 		echo " ** Already installed"
 	fi
 }
-function __feature_automake_1_14() {
+function __feature_autotools_automake_1_14() {
 	FEATURE_TEST="$STELLA_APP_FEATURE_ROOT/autotools/bin/automake"
 	FEATURE_RESULT_ROOT="$STELLA_APP_FEATURE_ROOT/autotools"
 	FEATURE_RESULT_PATH="$FEATURE_RESULT_ROOT/bin"
@@ -143,7 +148,7 @@ function __feature_automake_1_14() {
 
 
 
-function __install_libtool_2_4_2() {
+function __install_autotools_libtool_2_4_2() {
 	URL=http://ftp.gnu.org/gnu/libtool/libtool-2.4.2.tar.gz
 	VER=2.4.2
 	FILE_NAME=libtool-2.4.2.tar.gz
@@ -154,7 +159,7 @@ function __install_libtool_2_4_2() {
 	AUTO_INSTALL_FLAG_PREFIX=
 	AUTO_INSTALL_FLAG_POSTFIX=
 
-	feature_libtool_2_4_2
+	feature_autotools_libtool_2_4_2
 	[ "$FORCE" ] && TEST_FEATURE=0
 	if [ "$TEST_FEATURE" == "0" ]; then
 		__auto_install "configure" "libtool" "$FILE_NAME" "$URL" "$SRC_DIR" "$BUILD_DIR" "$INSTALL_DIR" "STRIP"
@@ -162,7 +167,7 @@ function __install_libtool_2_4_2() {
 		echo " ** Already installed"
 	fi
 }
-function __feature_libtool_2_4_2() {
+function __feature_autotools_libtool_2_4_2() {
 	FEATURE_TEST="$STELLA_APP_FEATURE_ROOT/autotools/bin/libtool"
 	FEATURE_RESULT_ROOT="$STELLA_APP_FEATURE_ROOT/autotools"
 	FEATURE_RESULT_PATH="$FEATURE_RESULT_ROOT/bin"
@@ -178,7 +183,7 @@ function __feature_libtool_2_4_2() {
 
 
 
-function __install_m4_1_4_17() {
+function __install_autotools_m4_1_4_17() {
 	URL=http://ftp.gnu.org/gnu/m4/m4-1.4.17.tar.gz
 	VER=1.4.17
 	FILE_NAME=m4-1.4.17.tar.gz
@@ -189,7 +194,7 @@ function __install_m4_1_4_17() {
 	AUTO_INSTALL_FLAG_PREFIX=
 	AUTO_INSTALL_FLAG_POSTFIX=
 
-	__feature_m4_1_4_17
+	__feature_autotools_m4_1_4_17
 	[ "$FORCE" ] && TEST_FEATURE=0
 	if [ "$TEST_FEATURE" == "0" ]; then
 		__auto_install "configure" "m4" "$FILE_NAME" "$URL" "$SRC_DIR" "$BUILD_DIR" "$INSTALL_DIR" "STRIP"
@@ -197,7 +202,7 @@ function __install_m4_1_4_17() {
 		echo " ** Already installed"
 	fi
 }
-function __feature_m4_1_4_17() {
+function __feature_autotools_m4_1_4_17() {
 	FEATURE_TEST="$STELLA_APP_FEATURE_ROOT/autotools/bin/m4"
 	FEATURE_RESULT_ROOT="$STELLA_APP_FEATURE_ROOT/autotools"
 	FEATURE_RESULT_PATH="$FEATURE_RESULT_ROOT/bin"
