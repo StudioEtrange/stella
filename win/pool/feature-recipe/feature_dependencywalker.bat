7ac5791dc30b240c1f68a93a42ebf7ec55bb966f
nomorgan <nomorgan@gmail.com>
2015-05-18 04:08:18 +0200

    win features dependency walker, nikpeviewer

---
+++ win/pool/feature-recipe/feature_dependencywalker.bat
@@ -0,0 +1,54 @@
+@echo off
+call %*
+goto :eof
+
+
+
+:feature_dependencywalker
+	set "FEAT_NAME=dependencywalker"
+	set "FEAT_LIST_SCHEMA=2_2@x64/binary 2_2@x86/binary"
+	set "FEAT_DEFAULT_VERSION=2_2"
+	set "FEAT_DEFAULT_ARCH=x64"
+	set "FEAT_DEFAULT_FLAVOUR=binary"
+	
+goto :eof
+
+
+:feature_dependencywalker_2_2
+	set "FEAT_VERSION=2_2"
+
+	set FEAT_SOURCE_URL=
+	set FEAT_SOURCE_URL_FILENAME=
+	set FEAT_SOURCE_PATCH_CALLBACK=
+	set "FEAT_BINARY_URL_x64=http://www.dependencywalker.com/depends22_x64.zip"
+	set "FEAT_BINARY_URL_FILENAME_x64=depends22_x64.zip"
+	set "FEAT_BINARY_URL_x86=http://www.dependencywalker.com/depends22_x86.zip"
+	set "FEAT_BINARY_URL_FILENAME_x86=depends22_x86.zip"
+	set FEAT_BINARY_CALLBACK=
+
+	set FEAT_DEPENDENCIES=
+	set "FEAT_INSTALL_TEST=!FEAT_INSTALL_ROOT!\depends.exe"
+	set "FEAT_SEARCH_PATH=!FEAT_INSTALL_ROOT!"
+	set FEAT_ENV=
+
+	set FEAT_BUNDLE_LIST=
+goto :eof
+
+
+
+
+
+:feature_dependencywalker_install_binary
+	set "INSTALL_DIR=!FEAT_INSTALL_ROOT!"
+	set SRC_DIR=
+	set BUILD_DIR=
+
+	call %STELLA_COMMON%\common.bat :download_uncompress "%FEAT_BINARY_URL%" "%FEAT_BINARY_URL_FILENAME%" "%INSTALL_DIR%" "DEST_ERASE STRIP"
+		
+goto :eof
+
+
+
+
+
+
---
+++ win/pool/feature-recipe/feature_nikpeviewer.bat
@@ -0,0 +1,52 @@
+@echo off
+call %*
+goto :eof
+
+
+
+:feature_nikpeviewer
+	set "FEAT_NAME=nikpeviewer"
+	set "FEAT_LIST_SCHEMA=0_21@x86/binary"
+	set "FEAT_DEFAULT_VERSION=0_21"
+	set "FEAT_DEFAULT_ARCH=x86"
+	set "FEAT_DEFAULT_FLAVOUR=binary"
+	
+goto :eof
+
+
+:feature_nikpeviewer_0_21
+	set "FEAT_VERSION=0_21"
+
+	set FEAT_SOURCE_URL=
+	set FEAT_SOURCE_URL_FILENAME=
+	set FEAT_SOURCE_PATCH_CALLBACK=
+	set "FEAT_BINARY_URL_x86=http://www.codedebug.com/Downloads/Download.php?file=NikPEViewer_21v.zip"
+	set "FEAT_BINARY_URL_FILENAME_x86=NikPEViewer_21v.zip"
+	set FEAT_BINARY_CALLBACK=
+
+	set FEAT_DEPENDENCIES=
+	set "FEAT_INSTALL_TEST=!FEAT_INSTALL_ROOT!\NikPEViewer.exe"
+	set "FEAT_SEARCH_PATH=!FEAT_INSTALL_ROOT!"
+	set FEAT_ENV=
+
+	set FEAT_BUNDLE_LIST=
+goto :eof
+
+
+
+
+
+:feature_nikpeviewer_install_binary
+	set "INSTALL_DIR=!FEAT_INSTALL_ROOT!"
+	set SRC_DIR=
+	set BUILD_DIR=
+
+	call %STELLA_COMMON%\common.bat :download_uncompress "%FEAT_BINARY_URL%" "%FEAT_BINARY_URL_FILENAME%" "%INSTALL_DIR%" "DEST_ERASE"
+		
+goto :eof
+
+
+
+
+
+
