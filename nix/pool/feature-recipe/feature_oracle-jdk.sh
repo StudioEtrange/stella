7b5188fea10cd0618ce7cf7beb681f147fc3eec3
StudioEtrange <nomorgan@gmail.com>
2015-05-23 05:04:29 +0200

    oracle jdk

--- nix/pool/feature-recipe/feature_oracle-jdk.sh
+++ nix/pool/feature-recipe/feature_oracle-jdk.sh
@@ -39,6 +39,9 @@ function feature_oracle-jdk_8u45() {
 		FEAT_BINARY_URL_FILENAME_x64=jdk-8u45-macosx-x64.dmg
 		FEAT_BINARY_URL_x86=
 		FEAT_BINARY_URL_FILENAME_86=
+
+		DMG_VOLUME_NAME="JDK 8 Update 45"
+		PKG_NAME="JDK 8 Update 45.pkg"
 	fi
 
 	FEAT_DEPENDENCIES=
@@ -62,6 +65,12 @@ function feature_oracle-jdk_7u80() {
 		FEAT_BINARY_URL_FILENAME_86=jdk-7u80-linux-i586.tar.gz
 	fi
 
+	if [ "$STELLA_CURRENT_PLATFORM" == "darwin" ]; then
+		FEAT_BINARY_URL_x64="http://download.oracle.com/otn-pub/java/jdk/7u80-b15/jdk-7u80-macosx-x64.dmg"
+		FEAT_BINARY_URL_FILENAME_x64=jdk-7u80-macosx-x64.dmg
+		FEAT_BINARY_URL_x86=
+		FEAT_BINARY_URL_FILENAME_86=
+	fi
 
 	FEAT_DEPENDENCIES=
 	FEAT_INSTALL_TEST=$FEAT_INSTALL_ROOT/bin/java
@@ -79,16 +88,48 @@ function feature_oracle-jdk_install_binary() {
 	SRC_DIR=
 	BUILD_DIR=
 
-	if [ ! -f "$STELLA_APP_CACHE_DIR/$FEAT_BINARY_URL_FILENAME" ]; then
+	
 
-		if [ "$STELLA_CURRENT_PLATFORM" == "linux" ]; then
+	if [ "$STELLA_CURRENT_PLATFORM" == "linux" ]; then
+		if [ ! -f "$STELLA_APP_CACHE_DIR/$FEAT_BINARY_URL_FILENAME" ]; then
 			wget --no-cookies --no-check-certificate --header Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie "$FEAT_BINARY_URL" -O "$STELLA_APP_CACHE_DIR%/$FEAT_BINARY_URL_FILENAME"
-			__uncompress "$STELLA_APP_CACHE_DIR/$FEAT_BINARY_URL_FILENAME" "$FEAT_INSTALL_ROOT" "DEST_ERASE"
 		fi
-		if [ "$STELLA_CURRENT_PLATFORM" == "darwin" ]; then
+		__uncompress "$STELLA_APP_CACHE_DIR/$FEAT_BINARY_URL_FILENAME" "$FEAT_INSTALL_ROOT" "DEST_ERASE"
+	fi
+	
+
+
+
+	if [ "$STELLA_CURRENT_PLATFORM" == "darwin" ]; then
+		# download
+		if [ ! -f "$STELLA_APP_CACHE_DIR/$FEAT_BINARY_URL_FILENAME" ]; then
 			curl -j -k -S -L -H "Cookie: oraclelicense=accept-securebackup-cookie; oraclelicense=accept-securebackup-cookie" -o "$STELLA_APP_CACHE_DIR/$FEAT_BINARY_URL_FILENAME" "$FEAT_BINARY_URL"
 		fi
+
+		# mount dmg file and extract pkg file
+		if [ ! -f "$STELLA_APP_CACHE_DIR/$PKG_NAME" ]; then
+			hdiutil mount "$STELLA_APP_CACHE_DIR/$FEAT_BINARY_URL_FILENAME"
+			cp "/Volumes/$DMG_VOLUME_NAME/$PKG_NAME" "$STELLA_APP_CACHE_DIR/$PKG_NAME"
+			hdiutil unmount "/Volumes/$DMG_VOLUME_NAME"
+		fi
+
+		# unzip pkg file
+		rm -Rf "$STELLA_APP_TEMP_DIR/$FEAT_VERSION"
+		pkgutil --expand "$STELLA_APP_CACHE_DIR/$PKG_NAME" "$STELLA_APP_TEMP_DIR/$FEAT_VERSION/"
+
+		# extract jdk Payload from pkg file
+		rm -Rf "$FEAT_INSTALL_ROOT"
+		mkdir -p "$FEAT_INSTALL_ROOT"
+		cd "$FEAT_INSTALL_ROOT"
+		for payload in "$STELLA_APP_TEMP_DIR"/$FEAT_VERSION/jdk*; do
+			tar xvzf "$payload/Payload"
+		done
+		__copy_folder_content_into "$FEAT_INSTALL_ROOT/Contents/Home" "$FEAT_INSTALL_ROOT"
+
+		rm -Rf "$FEAT_INSTALL_ROOT/Contents"
+		rm -Rf "$STELLA_APP_TEMP_DIR/$FEAT_VERSION"
 	fi
+
 
 }
 
