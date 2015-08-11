bb5a4d9b0f92d3555ce1566e97d395d86e65f0fd
StudioEtrange <nomorgan@gmail.com>
2015-08-12 01:12:32 +0200

    nix recipe python

--- nix/pool/feature-recipe/feature_stlport.sh
+++ nix/pool/feature-recipe/feature_stlport.sh
@@ -1,26 +1,22 @@
-if [ ! "$_PYTHON_INCLUDED_" == "1" ]; then 
-_PYTHON_INCLUDED_=1
+if [ ! "$_STLPORT_INCLUDED_" == "1" ]; then 
+_STLPORT_INCLUDED_=1
 
-# Python 2.7.9 and later (on the python2 series), and Python 3.4 and later include PIP by default 
-# But not python from code source
-# (http://pip.readthedocs.org/en/latest/installing.html#pip-included-with-python)
+function feature_stlport() {
 
-function feature_python() {
-
-	FEAT_NAME=python
+	FEAT_NAME=stlport
 	FEAT_LIST_SCHEMA="2_7_9:source"
 	FEAT_DEFAULT_VERSION=2_7_9
 	FEAT_DEFAULT_ARCH=
 	FEAT_DEFAULT_FLAVOUR="source"
 }
 
-function feature_python_2_7_9() {
+function feature_stlport_2_7_9() {
 	FEAT_VERSION=2_7_9
 	
-	FEAT_SOURCE_DEPENDENCIES="zlib#1_2_8 FORCE_ORIGIN_STELLA openssl#1_0_2d"
+	FEAT_SOURCE_DEPENDENCIES=
 	FEAT_BINARY_DEPENDENCIES=
 
-	FEAT_SOURCE_URL=https://www.python.org/ftp/python/2.7.9/Python-2.7.9.tgz
+	FEAT_SOURCE_URL=https://www.stlport.org/ftp/stlport/2.7.9/Python-2.7.9.tgz
 	FEAT_SOURCE_URL_FILENAME=Python-2.7.9.tgz
 	FEAT_SOURCE_URL_PROTOCOL=HTTP_ZIP
 
@@ -28,31 +24,31 @@ function feature_python_2_7_9() {
 	FEAT_BINARY_URL_FILENAME=
 	FEAT_BINARY_URL_PROTOCOL=
 
-	FEAT_SOURCE_CALLBACK=feature_python_link
+	FEAT_SOURCE_CALLBACK=feature_stlport_link
 	FEAT_BINARY_CALLBACK=
 	FEAT_ENV_CALLBACK=
 
-	FEAT_INSTALL_TEST="$FEAT_INSTALL_ROOT"/bin/python
+	FEAT_INSTALL_TEST="$FEAT_INSTALL_ROOT"/bin/stlport
 	FEAT_SEARCH_PATH="$FEAT_INSTALL_ROOT"/bin
 
 }
 
 
 
-function feature_python_link() {
+function feature_stlport_link() {
 	__link_feature_library "zlib#1_2_8" "z"
 	__link_feature_library "FORCE_ORIGIN_STELLA openssl#1_0_2d"
 }
 
 
-function feature_python_install_source() {
+function feature_stlport_install_source() {
 	INSTALL_DIR="$FEAT_INSTALL_ROOT"
 	SRC_DIR="$STELLA_APP_FEATURE_ROOT/$FEAT_NAME-$FEAT_VERSION-src"
 
 	# AUTO_INSTALL_FLAG_POSTFIX="--disable-dependency-tracking \
  #                          --enable-utf8 \
  #							--enable-ipv6"
-	#--with-ensurepip=install # build pip from pip source included into python source BUT need openssl
+	#--with-ensurepip=install # build pip from pip source included into stlport source BUT need openssl
 
 	__get_resource "$FEAT_NAME" "$FEAT_SOURCE_URL" "$FEAT_SOURCE_URL_PROTOCOL" "$SRC_DIR" "DEST_ERASE STRIP"
 
@@ -67,7 +63,7 @@ function feature_python_install_source() {
 	AUTO_INSTALL_BUILD_FLAG_PREFIX=
 	AUTO_INSTALL_BUILD_FLAG_POSTFIX=
 
-	# fix min macos version, information needed for building python
+	# fix min macos version, information needed for building stlport
 	# TODO
 	[ "$STELLA_CURRENT_OS" == "macos" ] && __set_build_mode MACOSX_DEPLOYMENT_TARGET $(__get_macos_version)
 
@@ -76,7 +72,7 @@ function feature_python_install_source() {
 	# install last pip/setuptools
 	__get_resource "get-pip" "https://bootstrap.pypa.io/get-pip.py" "HTTP" "$FEAT_INSTALL_ROOT/pip"
 	cd "$FEAT_INSTALL_ROOT/pip"
-	"$FEAT_INSTALL_ROOT/bin/python" get-pip.py
+	"$FEAT_INSTALL_ROOT/bin/stlport" get-pip.py
 	rm -Rf "$FEAT_INSTALL_ROOT/pip"
 
 }
