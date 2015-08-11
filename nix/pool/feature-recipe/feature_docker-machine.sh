6d3a26f1a15967dbea1ee94f21abf4e4a6f8202d
StudioEtrange <nomorgan@gmail.com>
2015-08-11 14:28:55 +0200

    nix build system

--- nix/pool/feature-recipe/feature_docker-machine.sh
+++ nix/pool/feature-recipe/feature_docker-machine.sh
@@ -4,8 +4,8 @@ _DOCKERMACHINE_INCLUDED_=1
 
 function feature_docker-machine() {
 	FEAT_NAME=docker-machine
-	FEAT_LIST_SCHEMA="0_2_0@x64:binary 0_2_0@x86:binary"
-	FEAT_DEFAULT_VERSION=0_2_0
+	FEAT_LIST_SCHEMA="0_2_0@x64:binary 0_2_0@x86:binary 0_4_0@x86:binary 0_4_0@x64:binary"
+	FEAT_DEFAULT_VERSION=0_4_0
 	FEAT_DEFAULT_ARCH=x64
 	FEAT_DEFAULT_FLAVOUR=binary
 }
@@ -36,6 +36,45 @@ function feature_docker-machine_0_2_0() {
 		FEAT_BINARY_URL_PROTOCOL_x86=HTTP
 		FEAT_BINARY_URL_x64=https://github.com/docker/machine/releases/download/v0.2.0/docker-machine_linux-amd64
 		FEAT_BINARY_URL_FILENAME_x64=docker-machine_linux-amd64-0_2_0
+		FEAT_BINARY_URL_PROTOCOL_x64=HTTP
+	fi
+
+	FEAT_SOURCE_CALLBACK=
+	FEAT_BINARY_CALLBACK=
+	FEAT_ENV_CALLBACK=
+
+	FEAT_INSTALL_TEST="$FEAT_INSTALL_ROOT"/docker-machine
+	FEAT_SEARCH_PATH="$FEAT_INSTALL_ROOT"
+	
+}
+
+
+function feature_docker-machine_0_4_0() {
+	FEAT_VERSION=0_4_0
+	
+	FEAT_SOURCE_DEPENDENCIES=
+	FEAT_BINARY_DEPENDENCIES=
+	
+
+	FEAT_SOURCE_URL=
+	FEAT_SOURCE_URL_FILENAME=
+	FEAT_SOURCE_URL_PROTOCOL=
+	
+	if [ "$STELLA_CURRENT_PLATFORM" == "darwin" ]; then
+		FEAT_BINARY_URL_x86=https://github.com/docker/machine/releases/download/v0.4.0/docker-machine_darwin-386
+		FEAT_BINARY_URL_FILENAME_x86=docker-machine_darwin-386-0_4_0
+		FEAT_BINARY_URL_PROTOCOL_x86=HTTP
+		FEAT_BINARY_URL_x64=https://github.com/docker/machine/releases/download/v0.4.0/docker-machine_darwin-amd64
+		FEAT_BINARY_URL_FILENAME_x64=docker-machine_darwin-amd64-0_4_0
+		FEAT_BINARY_URL_PROTOCOL_x64=HTTP
+	fi
+
+	if [ "$STELLA_CURRENT_PLATFORM" == "linux" ]; then
+		FEAT_BINARY_URL_x86=https://github.com/docker/machine/releases/download/v0.4.0/docker-machine_linux-386
+		FEAT_BINARY_URL_FILENAME_x86=docker-machine_linux-386-0_4_0
+		FEAT_BINARY_URL_PROTOCOL_x86=HTTP
+		FEAT_BINARY_URL_x64=https://github.com/docker/machine/releases/download/v0.4.0/docker-machine_linux-amd64
+		FEAT_BINARY_URL_FILENAME_x64=docker-machine_linux-amd64-0_4_0
 		FEAT_BINARY_URL_PROTOCOL_x64=HTTP
 	fi
 
