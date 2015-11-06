66b058f2b9c1b0596eaaf5e7acb9d27d7108dd5f
StudioEtrange <nomorgan@gmail.com>
2015-11-06 12:36:29 +0100

    es kibana update

--- nix/pool/feature-recipe/feature_elasticsearch.sh
+++ nix/pool/feature-recipe/feature_elasticsearch.sh
@@ -5,8 +5,8 @@ _ELASTICSEARCH_INCLUDED_=1
 
 function feature_elasticsearch() {
 	FEAT_NAME=elasticsearch
-	FEAT_LIST_SCHEMA="1_4_4:binary 1_5_0:binary 1_6_0:binary"
-	FEAT_DEFAULT_VERSION=1_5_0
+	FEAT_LIST_SCHEMA="1_4_4:binary 1_5_0:binary 1_5_2:binary 1_6_0:binary 1_6_2:binary 1_7_3:binary 2_0_0:binary"
+	FEAT_DEFAULT_VERSION=1_7_3
 	FEAT_DEFAULT_ARCH=
 	FEAT_DEFAULT_FLAVOUR="binary"
 }
@@ -15,6 +15,77 @@ function feature_elasticsearch() {
 function feature_elasticsearch_env() {
 	ES_HOME=$FEAT_INSTALL_ROOT
 	export ES_HOME=$FEAT_INSTALL_ROOT
+}
+
+
+function feature_elasticsearch_2_0_0() {
+	FEAT_VERSION=2_0_0
+	# TODO NEED JDK
+	FEAT_SOURCE_DEPENDENCIES=
+	FEAT_BINARY_DEPENDENCIES=
+	
+	FEAT_SOURCE_URL=
+	FEAT_SOURCE_URL_FILENAME=
+	FEAT_SOURCE_URL_PROTOCOL=
+
+	FEAT_BINARY_URL=https://download.elasticsearch.org/elasticsearch/release/org/elasticsearch/distribution/tar/elasticsearch/2.0.0/elasticsearch-2.0.0.tar.gz
+	FEAT_BINARY_URL_FILENAME=elasticsearch-2.0.0.tar.gz
+	FEAT_BINARY_URL_PROTOCOL=HTTP_ZIP
+
+	FEAT_SOURCE_CALLBACK=
+	FEAT_BINARY_CALLBACK=
+	FEAT_ENV_CALLBACK=feature_elasticsearch_env
+
+	FEAT_INSTALL_TEST="$FEAT_INSTALL_ROOT"/bin/elasticsearch
+	FEAT_SEARCH_PATH="$FEAT_INSTALL_ROOT"/bin
+
+}
+
+
+function feature_elasticsearch_1_7_3() {
+	FEAT_VERSION=1_7_3
+	# TODO NEED JDK
+	FEAT_SOURCE_DEPENDENCIES=
+	FEAT_BINARY_DEPENDENCIES=
+	
+	FEAT_SOURCE_URL=
+	FEAT_SOURCE_URL_FILENAME=
+	FEAT_SOURCE_URL_PROTOCOL=
+
+	FEAT_BINARY_URL=https://download.elastic.co/elasticsearch/elasticsearch/elasticsearch-1.7.3.tar.gz
+	FEAT_BINARY_URL_FILENAME=elasticsearch-1.7.3.tar.gz
+	FEAT_BINARY_URL_PROTOCOL=HTTP_ZIP
+
+	FEAT_SOURCE_CALLBACK=
+	FEAT_BINARY_CALLBACK=
+	FEAT_ENV_CALLBACK=feature_elasticsearch_env
+
+	FEAT_INSTALL_TEST="$FEAT_INSTALL_ROOT"/bin/elasticsearch
+	FEAT_SEARCH_PATH="$FEAT_INSTALL_ROOT"/bin
+
+}
+
+function feature_elasticsearch_1_6_2() {
+	FEAT_VERSION=1_6_2
+	# TODO NEED JDK
+	FEAT_SOURCE_DEPENDENCIES=
+	FEAT_BINARY_DEPENDENCIES=
+	
+	FEAT_SOURCE_URL=
+	FEAT_SOURCE_URL_FILENAME=
+	FEAT_SOURCE_URL_PROTOCOL=
+
+	FEAT_BINARY_URL=https://download.elastic.co/elasticsearch/elasticsearch/elasticsearch-1.6.2.tar.gz
+	FEAT_BINARY_URL_FILENAME=elasticsearch-1.6.2.tar.gz
+	FEAT_BINARY_URL_PROTOCOL=HTTP_ZIP
+
+	FEAT_SOURCE_CALLBACK=
+	FEAT_BINARY_CALLBACK=
+	FEAT_ENV_CALLBACK=feature_elasticsearch_env
+
+	FEAT_INSTALL_TEST="$FEAT_INSTALL_ROOT"/bin/elasticsearch
+	FEAT_SEARCH_PATH="$FEAT_INSTALL_ROOT"/bin
+
 }
 
 function feature_elasticsearch_1_4_4() {
@@ -29,6 +100,7 @@ function feature_elasticsearch_1_4_4() {
 
 	FEAT_BINARY_URL=https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-1.4.4.tar.gz
 	FEAT_BINARY_URL_FILENAME=elasticsearch-1.4.4.tar.gz
+	FEAT_BINARY_URL_PROTOCOL=HTTP_ZIP
 
 	FEAT_SOURCE_CALLBACK=
 	FEAT_BINARY_CALLBACK=
@@ -62,6 +134,31 @@ function feature_elasticsearch_1_5_0() {
 	FEAT_SEARCH_PATH="$FEAT_INSTALL_ROOT"/bin
 
 }
+
+
+function feature_elasticsearch_1_5_2() {
+	FEAT_VERSION=1_5_2
+	# TODO NEED JDK 7 >= 7.60
+	FEAT_SOURCE_DEPENDENCIES=
+	FEAT_BINARY_DEPENDENCIES=
+
+	FEAT_SOURCE_URL=
+	FEAT_SOURCE_URL_FILENAME=
+	FEAT_SOURCE_URL_PROTOCOL=
+	
+	FEAT_BINARY_URL=https://download.elastic.co/elasticsearch/elasticsearch/elasticsearch-1.5.2.tar.gz
+	FEAT_BINARY_URL_FILENAME=elasticsearch-1.5.2.tar.gz
+	FEAT_BINARY_URL_PROTOCOL=HTTP_ZIP
+
+	FEAT_SOURCE_CALLBACK=
+	FEAT_BINARY_CALLBACK=
+	FEAT_ENV_CALLBACK=feature_elasticsearch_env
+
+	FEAT_INSTALL_TEST="$FEAT_INSTALL_ROOT"/bin/elasticsearch
+	FEAT_SEARCH_PATH="$FEAT_INSTALL_ROOT"/bin
+
+}
+
 
 
 function feature_elasticsearch_1_6_0() {
--- nix/pool/feature-recipe/feature_kibana.sh
+++ nix/pool/feature-recipe/feature_kibana.sh
@@ -2,15 +2,83 @@ if [ ! "$_KIBANA_INCLUDED_" == "1" ]; then
 _KIBANA_INCLUDED_=1
 
 
-
+# NOTE : 
+#		KIBANA 4.2 NEED ES 2.X
+#		KIBANA 4.1.2 NEED ES 1.4.4 - 1.7
 function feature_kibana() {
 	FEAT_NAME=kibana
-	FEAT_LIST_SCHEMA="4_0_0:binary 3_1_2:source 4_0_1:binary 4_0_3:binary 4_1_0:binary"
-	FEAT_DEFAULT_VERSION=4_0_0
+	FEAT_LIST_SCHEMA="4_0_0:binary 3_1_2:source 4_0_1:binary 4_0_3:binary 4_1_0:binary 4_1_2:binary 4_2_0:binary"
+	FEAT_DEFAULT_VERSION=4_1_2
 	FEAT_DEFAULT_ARCH=
 	FEAT_DEFAULT_FLAVOUR="binary"
 }
 
+
+
+
+
+function feature_kibana_4_2_0() {
+	FEAT_VERSION=4_2_0
+	
+	FEAT_SOURCE_DEPENDENCIES=
+	FEAT_BINARY_DEPENDENCIES=
+
+	FEAT_SOURCE_URL=
+	FEAT_SOURCE_URL_FILENAME=
+	FEAT_SOURCE_URL_PROTOCOL=
+
+	if [ "$STELLA_CURRENT_PLATFORM" == "darwin" ]; then 
+		FEAT_BINARY_URL=https://download.elastic.co/kibana/kibana/kibana-4.2.0-darwin-x64.tar.gz
+		FEAT_BINARY_URL_FILENAME=kibana-4.2.0-darwin-x64.tar.gz
+		FEAT_BINARY_URL_PROTOCOL=HTTP_ZIP
+	fi
+
+	if [ "$STELLA_CURRENT_PLATFORM" == "linux" ]; then
+		FEAT_BINARY_URL=https://download.elastic.co/kibana/kibana/kibana-4.2.0-linux-x64.tar.gz
+		FEAT_BINARY_URL_FILENAME=kibana-4.2.0-linux-x64.tar.gz
+		FEAT_BINARY_URL_PROTOCOL=HTTP_ZIP
+	fi
+
+	FEAT_SOURCE_CALLBACK=
+	FEAT_BINARY_CALLBACK=
+	FEAT_ENV_CALLBACK=
+
+	FEAT_INSTALL_TEST="$FEAT_INSTALL_ROOT"/bin/kibana
+	FEAT_SEARCH_PATH="$FEAT_INSTALL_ROOT"/bin
+
+}
+
+
+function feature_kibana_4_1_2() {
+	FEAT_VERSION=4_1_2
+	
+	FEAT_SOURCE_DEPENDENCIES=
+	FEAT_BINARY_DEPENDENCIES=
+
+	FEAT_SOURCE_URL=
+	FEAT_SOURCE_URL_FILENAME=
+	FEAT_SOURCE_URL_PROTOCOL=
+
+	if [ "$STELLA_CURRENT_PLATFORM" == "darwin" ]; then 
+		FEAT_BINARY_URL=https://download.elastic.co/kibana/kibana/kibana-4.1.2-darwin-x64.zip
+		FEAT_BINARY_URL_FILENAME=kibana-4.1.2-darwin-x64.zip
+		FEAT_BINARY_URL_PROTOCOL=HTTP_ZIP
+	fi
+
+	if [ "$STELLA_CURRENT_PLATFORM" == "linux" ]; then
+		FEAT_BINARY_URL=https://download.elastic.co/kibana/kibana/kibana-4.1.2-linux-x64.tar.gz
+		FEAT_BINARY_URL_FILENAME=kibana-4.1.2-linux-x64.tar.gz
+		FEAT_BINARY_URL_PROTOCOL=HTTP_ZIP
+	fi
+
+	FEAT_SOURCE_CALLBACK=
+	FEAT_BINARY_CALLBACK=
+	FEAT_ENV_CALLBACK=
+
+	FEAT_INSTALL_TEST="$FEAT_INSTALL_ROOT"/bin/kibana
+	FEAT_SEARCH_PATH="$FEAT_INSTALL_ROOT"/bin
+
+}
 
 function feature_kibana_3_1_2() {
 	FEAT_VERSION=3_1_2
@@ -164,6 +232,9 @@ function feature_kibana_4_1_0() {
 	FEAT_SEARCH_PATH="$FEAT_INSTALL_ROOT"/bin
 
 }
+
+
+
 
 
 
