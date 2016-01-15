4d3341f03885630da56ebc4f5d17461d873c2703
StudioEtrange <nomorgan@gmail.com>
2016-01-15 01:14:52 +0100

    go versions

--- nix/pool/feature-recipe/feature_go.sh
+++ nix/pool/feature-recipe/feature_go.sh
@@ -5,7 +5,7 @@ _GO_INCLUDED_=1
 function feature_go() {
 
 	FEAT_NAME=go
-	FEAT_LIST_SCHEMA="1_4_3:source 1_4_3:binary 1_5_3:source 1_5_3:binary"
+	FEAT_LIST_SCHEMA="1_4_2:source 1_4_2:binary 1_4_3:source 1_4_3:binary 1_5_3:source 1_5_3:binary"
 	FEAT_DEFAULT_VERSION=1_4_3
 	FEAT_DEFAULT_ARCH=
 	FEAT_DEFAULT_FLAVOUR="source"
@@ -66,6 +66,36 @@ function feature_go_1_4_3() {
 	if [ "$STELLA_CURRENT_PLATFORM" == "linux" ]; then
 		FEAT_BINARY_URL=https://storage.googleapis.com/golang/go1.4.3.linux-amd64.tar.gz
 		FEAT_BINARY_URL_FILENAME=go1.4.3.linux-amd64.tar.gz
+		FEAT_BINARY_URL_PROTOCOL=HTTP_ZIP
+	fi
+
+	FEAT_SOURCE_CALLBACK=
+	FEAT_BINARY_CALLBACK=
+	FEAT_ENV_CALLBACK="go_set_env"
+
+	FEAT_INSTALL_TEST="$FEAT_INSTALL_ROOT"/bin/go
+	FEAT_SEARCH_PATH="$FEAT_INSTALL_ROOT"/bin
+}
+
+
+function feature_go_1_4_2() {
+	FEAT_VERSION=1_4_2
+
+	FEAT_SOURCE_DEPENDENCIES=
+	FEAT_BINARY_DEPENDENCIES=
+
+	FEAT_SOURCE_URL=https://storage.googleapis.com/golang/go1.4.2.src.tar.gz
+	FEAT_SOURCE_URL_FILENAME=go1.4.2.src.tar.gz
+	FEAT_SOURCE_URL_PROTOCOL=HTTP_ZIP
+
+	if [ "$STELLA_CURRENT_PLATFORM" == "darwin" ]; then
+		FEAT_BINARY_URL=https://storage.googleapis.com/golang/go1.4.2.darwin-amd64-osx10.8.tar.gz
+		FEAT_BINARY_URL_FILENAME=go1.4.2.darwin-amd64-osx10.8.tar.gz
+		FEAT_BINARY_URL_PROTOCOL=HTTP_ZIP
+	fi
+	if [ "$STELLA_CURRENT_PLATFORM" == "linux" ]; then
+		FEAT_BINARY_URL=https://storage.googleapis.com/golang/go1.4.2.linux-amd64.tar.gz
+		FEAT_BINARY_URL_FILENAME=go1.4.2.linux-amd64.tar.gz
 		FEAT_BINARY_URL_PROTOCOL=HTTP_ZIP
 	fi
 
