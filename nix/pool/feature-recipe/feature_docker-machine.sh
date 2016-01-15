7e04a7bea1e65a41fde34a30badc1370fde28589
StudioEtrange <nomorgan@gmail.com>
2016-01-15 01:20:42 +0100

    update docker components

--- nix/pool/feature-recipe/feature_docker-machine.sh
+++ nix/pool/feature-recipe/feature_docker-machine.sh
@@ -4,10 +4,50 @@ _DOCKERMACHINE_INCLUDED_=1
 
 function feature_docker-machine() {
 	FEAT_NAME=docker-machine
-	FEAT_LIST_SCHEMA="0_2_0@x64:binary 0_2_0@x86:binary 0_4_0@x86:binary 0_4_0@x64:binary"
-	FEAT_DEFAULT_VERSION=0_4_0
+	FEAT_LIST_SCHEMA="0_2_0@x64:binary 0_2_0@x86:binary 0_4_0@x86:binary 0_4_0@x64:binary 0_5_6@x64:binary"
+	FEAT_DEFAULT_VERSION=0_5_6
 	FEAT_DEFAULT_ARCH=x64
 	FEAT_DEFAULT_FLAVOUR=binary
+}
+
+
+
+function feature_docker-machine_0_5_6() {
+	FEAT_VERSION=0_5_6
+	
+	FEAT_SOURCE_DEPENDENCIES=
+	FEAT_BINARY_DEPENDENCIES=
+	
+
+	FEAT_SOURCE_URL=
+	FEAT_SOURCE_URL_FILENAME=
+	FEAT_SOURCE_URL_PROTOCOL=
+	
+	if [ "$STELLA_CURRENT_PLATFORM" == "darwin" ]; then
+		FEAT_BINARY_URL_x86=
+		FEAT_BINARY_URL_FILENAME_x86=
+		FEAT_BINARY_URL_PROTOCOL_x86=
+		FEAT_BINARY_URL_x64=https://github.com/docker/machine/releases/download/v0.5.6/docker-machine_darwin-amd64
+		FEAT_BINARY_URL_FILENAME_x64=docker-machine_darwin-amd64-0_56
+		FEAT_BINARY_URL_PROTOCOL_x64=HTTP
+	fi
+
+	if [ "$STELLA_CURRENT_PLATFORM" == "linux" ]; then
+		FEAT_BINARY_URL_x86=
+		FEAT_BINARY_URL_FILENAME_x86=
+		FEAT_BINARY_URL_PROTOCOL_x86=
+		FEAT_BINARY_URL_x64=https://github.com/docker/machine/releases/download/v0.5.6/docker-machine_linux-amd64
+		FEAT_BINARY_URL_FILENAME_x64=docker-machine_linux-amd64-0_5_6
+		FEAT_BINARY_URL_PROTOCOL_x64=HTTP
+	fi
+
+	FEAT_SOURCE_CALLBACK=
+	FEAT_BINARY_CALLBACK=
+	FEAT_ENV_CALLBACK=
+
+	FEAT_INSTALL_TEST="$FEAT_INSTALL_ROOT"/docker-machine
+	FEAT_SEARCH_PATH="$FEAT_INSTALL_ROOT"
+	
 }
 
 function feature_docker-machine_0_2_0() {
