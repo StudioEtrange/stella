41b3f762ea9ebcde58aa6e831c5a5b8d60aab925
StudioEtrange <nomorgan@gmail.com>
2016-08-23 03:20:11 +0200

    win : update mingw-w64

--- win/pool/feature-recipe/feature_mingw-w64.bat
+++ win/pool/feature-recipe/feature_mingw-w64.bat
@@ -4,18 +4,21 @@ goto :eof
 
 
 REM https://chocolatey.org/packages/mingw
+
+REM build some library with mingw-w64 :
 REM http://www.gaia-gis.it/spatialite-3.0.0-BETA/mingw64_how_to.html
 
-REM build from sourcee
+REM build from source
 REM https://ffmpeg.zeranoe.com/blog/?cat=4
 
 REM official website : http://mingw-w64.yaxm.org/doku.php/start
 REM download url from mingw-builds from : http://sourceforge.net/projects/mingw-w64/
+REM																				https://sourceforge.net/projects/mingw-w64/files/Toolchains%20targetting%20Win64/Personal%20Builds/mingw-builds/
 
 :feature_mingw-w64
 	set "FEAT_NAME=mingw-w64"
-	set "FEAT_LIST_SCHEMA=mingw4_gcc4_9_2_winthread@x64:binary mingw4_gcc4_9_2_winthread@x86:binary mingw4_gcc4_9_2_posixthread@x64:binary mingw4_gcc4_9_2_posixthread@x86:binary"
-	set "FEAT_DEFAULT_VERSION=mingw4_gcc4_9_2_winthread"
+	set "FEAT_LIST_SCHEMA=mingw4_gcc4_9_3_winthread@x64:binary mingw4_gcc4_9_3_winthread@x86:binary mingw4_gcc4_9_3_posixthread@x64:binary mingw4_gcc4_9_3_posixthread@x86:binary"
+	set "FEAT_DEFAULT_VERSION=mingw4_gcc4_9_3_winthread"
 	set "FEAT_DEFAULT_ARCH=x64"
 	set "FEAT_DEFAULT_FLAVOUR=binary"
 
@@ -23,8 +26,8 @@ goto :eof
 
 
 
-:feature_mingw-w64_mingw4_gcc4_9_2_winthread
-	set "FEAT_VERSION=mingw4_gcc4_9_2_winthread"
+:feature_mingw-w64_mingw4_gcc4_9_3_winthread
+	set "FEAT_VERSION=mingw4_gcc4_9_3_winthread"
 
 	set FEAT_SOURCE_DEPENDENCIES=
 	set FEAT_BINARY_DEPENDENCIES=
@@ -37,11 +40,11 @@ goto :eof
 
 	REM win32 or posix
 	set MINGW_THREADS=win32
-	set MINGW_GCC_VERSION=4.9.2
-	REM mingw branch 
+	set MINGW_GCC_VERSION=4.9.3
+	REM mingw branch
 	set MINGW_MAJOR_VERSION=4
 	REM package revision
-	set MINGW_ZIP_REVISION=2
+	set MINGW_ZIP_REVISION=1
 
 	set FEAT_SOURCE_URL=
 	set FEAT_SOURCE_URL_FILENAME=
@@ -63,13 +66,13 @@ goto :eof
 
 	set "FEAT_INSTALL_TEST=!FEAT_INSTALL_ROOT!\bin\gcc.exe"
 	set "FEAT_SEARCH_PATH=!FEAT_INSTALL_ROOT!\bin"
-	
+
 
 goto :eof
 
 
-:feature_mingw-w64_mingw4_gcc4_9_2_posixthread
-	set "FEAT_VERSION=mingw4_gcc4_9_2_posixthread"
+:feature_mingw-w64_mingw4_gcc4_9_3_posixthread
+	set "FEAT_VERSION=mingw4_gcc4_9_3_posixthread"
 
 	set FEAT_SOURCE_DEPENDENCIES=
 	set FEAT_BINARY_DEPENDENCIES=
@@ -82,11 +85,11 @@ goto :eof
 
 	REM win32 or posix
 	set MINGW_THREADS=posix
-	set MINGW_GCC_VERSION=4.9.2
-	REM mingw branch 
+	set MINGW_GCC_VERSION=4.9.3
+	REM mingw branch
 	set MINGW_MAJOR_VERSION=4
 	REM package revision
-	set MINGW_ZIP_REVISION=2
+	set MINGW_ZIP_REVISION=1
 
 	set FEAT_SOURCE_URL=
 	set FEAT_SOURCE_URL_FILENAME=
@@ -108,7 +111,7 @@ goto :eof
 
 	set "FEAT_INSTALL_TEST=!FEAT_INSTALL_ROOT!\bin\gcc.exe"
 	set "FEAT_SEARCH_PATH=!FEAT_INSTALL_ROOT!\bin"
-	
+
 
 goto :eof
 
@@ -116,6 +119,3 @@ goto :eof
 :feature_mingw-w64_install_binary
 	call %STELLA_COMMON%\common.bat :get_resource "!FEAT_NAME!" "!FEAT_BINARY_URL!" "!FEAT_BINARY_URL_PROTOCOL!" "!FEAT_INSTALL_ROOT!" "DEST_ERASE STRIP FORCE_NAME !FEAT_BINARY_URL_FILENAME!"
 goto :eof
-
-
-
