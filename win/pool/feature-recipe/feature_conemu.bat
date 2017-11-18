9cd911ed5394d852b16e62b63b5a82d46e971b18
nomorgan <nomorgan@gmail.com>
2017-11-18 21:27:22 +0100

    win : conemu update

--- win/pool/feature-recipe/feature_conemu.bat
+++ win/pool/feature-recipe/feature_conemu.bat
@@ -2,19 +2,20 @@
 call %*
 goto :eof
 
-
+::https://conemu.github.io/
 
 :feature_conemu
 	set "FEAT_NAME=conemu"
-	set "FEAT_LIST_SCHEMA=preview150307a:binary"
-	set "FEAT_DEFAULT_VERSION=preview150307a"
+	set "FEAT_LIST_SCHEMA=171109:binary 161206:binary"
+	set "FEAT_DEFAULT_VERSION=161206"
 	set FEAT_DEFAULT_ARCH=
 	set "FEAT_DEFAULT_FLAVOUR=binary"
 goto :eof
 
 
-:feature_conemu_preview150307a
-	set "FEAT_VERSION=preview150307a"
+:feature_conemu_171109
+	:: NOTE : preview version
+	set "FEAT_VERSION=171109"
 
 	set FEAT_SOURCE_DEPENDENCIES=
 	set FEAT_BINARY_DEPENDENCIES=
@@ -23,7 +24,32 @@ goto :eof
 	set FEAT_SOURCE_URL_FILENAME=
 	set FEAT_SOURCE_URL_PROTOCOL=
 
-	set "FEAT_BINARY_URL=http://heanet.dl.sourceforge.net/project/conemu/Preview/ConEmuPack.150307a.7z"
+	set "FEAT_BINARY_URL=https://downloads.sourceforge.net/project/conemu/Preview/ConEmuPack.171109.7z"
+	set FEAT_BINARY_URL_FILENAME=
+	set "FEAT_BINARY_URL_PROTOCOL=HTTP_ZIP"
+
+	set FEAT_SOURCE_CALLBACK=
+	set FEAT_BINARY_CALLBACK=
+	set FEAT_ENV_CALLBACK=
+
+	set "FEAT_INSTALL_TEST=!FEAT_INSTALL_ROOT!\ConEmu64.exe"
+	set "FEAT_SEARCH_PATH=!FEAT_INSTALL_ROOT!"
+goto :eof
+
+
+
+:feature_conemu_161206
+	:: NOTE : stable version
+	set "FEAT_VERSION=161206"
+
+	set FEAT_SOURCE_DEPENDENCIES=
+	set FEAT_BINARY_DEPENDENCIES=
+
+	set FEAT_SOURCE_URL=
+	set FEAT_SOURCE_URL_FILENAME=
+	set FEAT_SOURCE_URL_PROTOCOL=
+
+	set "FEAT_BINARY_URL=https://downloads.sourceforge.net/project/conemu/Stable/ConEmuPack.161206.7z"
 	set FEAT_BINARY_URL_FILENAME=
 	set "FEAT_BINARY_URL_PROTOCOL=HTTP_ZIP"
 
