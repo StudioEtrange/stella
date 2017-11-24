57b7a3d85703b828eff67e857a8f219f0fcb3a31
StudioEtrange <sboucault@gmail.com>
2017-11-24 11:31:24 +0100

    nix : heartbeat update

--- nix/pool/feature-recipe/feature_heartbeat.sh
+++ nix/pool/feature-recipe/feature_heartbeat.sh
@@ -45,6 +45,38 @@ feature_heartbeat_5_6_4() {
 
 
 
+feature_heartbeat_5_6_0() {
+	FEAT_VERSION=5_6_0
+
+	FEAT_SOURCE_DEPENDENCIES=
+	FEAT_BINARY_DEPENDENCIES=
+
+	FEAT_SOURCE_URL=
+	FEAT_SOURCE_URL_FILENAME=
+	FEAT_SOURCE_URL_PROTOCOL=
+
+	if [ "$STELLA_CURRENT_PLATFORM" = "darwin" ]; then
+		FEAT_BINARY_URL=https://artifacts.elastic.co/downloads/beats/heartbeat/heartbeat-5.6.0-darwin-x86_64.tar.gz
+		FEAT_BINARY_URL_FILENAME=heartbeat-5.6.0-darwin-x86_64.tar.gz
+		FEAT_BINARY_URL_PROTOCOL=HTTP_ZIP
+	fi
+
+	if [ "$STELLA_CURRENT_PLATFORM" = "linux" ]; then
+		FEAT_BINARY_URL=https://artifacts.elastic.co/downloads/beats/heartbeat/heartbeat-5.6.0-linux-x86_64.tar.gz
+		FEAT_BINARY_URL_FILENAME=heartbeat-5.6.0-linux-x86_64.tar.gz
+		FEAT_BINARY_URL_PROTOCOL=HTTP_ZIP
+	fi
+
+	FEAT_SOURCE_CALLBACK=
+	FEAT_BINARY_CALLBACK=
+	FEAT_ENV_CALLBACK=
+
+	FEAT_INSTALL_TEST="$FEAT_INSTALL_ROOT"/heartbeat
+	FEAT_SEARCH_PATH="$FEAT_INSTALL_ROOT"
+
+}
+
+
 feature_heartbeat_install_binary() {
 	__get_resource "$FEAT_NAME" "$FEAT_BINARY_URL" "$FEAT_BINARY_URL_PROTOCOL" "$FEAT_INSTALL_ROOT" "DEST_ERASE STRIP"
 }
