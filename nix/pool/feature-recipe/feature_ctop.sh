b8e261778db3faf7bcce79758c7c747e3fbb7768
StudioEtrange <sboucault@gmail.com>
2017-11-26 00:18:36 +0100

    nix : fix ctop

--- nix/pool/feature-recipe/feature_ctop.sh
+++ nix/pool/feature-recipe/feature_ctop.sh
@@ -22,9 +22,18 @@ feature_ctop_0_6_1() {
 	FEAT_SOURCE_URL_FILENAME=
 	FEAT_SOURCE_URL_PROTOCOL=
 
-	FEAT_BINARY_URL=https://github.com/bcicen/ctop/releases/download/v0.6.1/ctop-0.6.1-linux-amd64
-	FEAT_BINARY_URL_FILENAME=ctop-0.6.1-linux-amd64
-	FEAT_BINARY_URL_PROTOCOL=HTTP
+	if [ "$STELLA_CURRENT_PLATFORM" = "linux" ]; then
+		FEAT_BINARY_URL=https://github.com/bcicen/ctop/releases/download/v0.6.1/ctop-0.6.1-linux-amd64
+		FEAT_BINARY_URL_FILENAME=ctop-0.6.1-linux-amd64
+		FEAT_BINARY_URL_PROTOCOL=HTTP
+	fi
+
+	if [ "$STELLA_CURRENT_PLATFORM" = "darwin" ]; then
+		FEAT_BINARY_URL=https://github.com/bcicen/ctop/releases/download/v0.6.1/ctop-0.6.1-darwin-amd64
+		FEAT_BINARY_URL_FILENAME=ctop-0.6.1-darwin-amd64
+		FEAT_BINARY_URL_PROTOCOL=HTTP
+	fi
+
 
 	FEAT_SOURCE_CALLBACK=
 	FEAT_BINARY_CALLBACK=
