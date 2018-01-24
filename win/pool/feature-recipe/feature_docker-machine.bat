a024119d9a4f15db64b6a4ed936d404a0871d302
nomorgan <nomorgan@gmail.com>
2018-01-24 03:17:13 +0100

    WIN : update docker recipes

--- win/pool/feature-recipe/feature_docker-machine.bat
+++ win/pool/feature-recipe/feature_docker-machine.bat
@@ -5,10 +5,36 @@ goto :eof
 
 :feature_docker-machine
 	set "FEAT_NAME=docker-machine"
-	set "FEAT_LIST_SCHEMA=0_8_2@x64:binary 0_8_2@x86:binary 0_2_0@x64:binary 0_2_0@x86:binary 0_4_0@x86:binary 0_4_0@x64:binary"
+	set "FEAT_LIST_SCHEMA=0_13_0@x64:binary 0_13_0@x86:binary 0_8_2@x64:binary 0_8_2@x86:binary 0_2_0@x64:binary 0_2_0@x86:binary 0_4_0@x86:binary 0_4_0@x64:binary"
 	set "FEAT_DEFAULT_VERSION=0_8_2"
 	set "FEAT_DEFAULT_ARCH=x64"
 	set "FEAT_DEFAULT_FLAVOUR=binary"
+goto :eof
+
+
+:feature_docker-machine_0_13_0
+	set "FEAT_VERSION=0_13_0"
+
+	set FEAT_SOURCE_DEPENDENCIES=
+	set FEAT_BINARY_DEPENDENCIES=
+
+	set FEAT_SOURCE_URL=
+	set FEAT_SOURCE_URL_FILENAME=
+	set FEAT_SOURCE_URL_PROTOCOL=
+
+	set "FEAT_BINARY_URL_x64=https://github.com/docker/machine/releases/download/v0.13.0/docker-machine-Windows-x86_64.exe"
+	set "FEAT_BINARY_URL_FILENAME_x64=docker-machine-Windows-x86_64-0_13_0.exe"
+	set FEAT_BINARY_URL_PROTOCOL_x64=HTTP
+	set "FEAT_BINARY_URL_x86=https://github.com/docker/machine/releases/download/v0.13.0/docker-machine-Windows-i386.exe"
+	set "FEAT_BINARY_URL_FILENAME_x86=docker-machine-Windows-i386-0_13_0.exe"
+	set FEAT_BINARY_URL_PROTOCOL_x86=HTTP
+
+	set FEAT_ENV_CALLBACK=
+	set FEAT_SOURCE_CALLBACK=
+	set FEAT_BINARY_CALLBACK=
+
+	set "FEAT_INSTALL_TEST=!FEAT_INSTALL_ROOT!\docker-machine.exe"
+	set "FEAT_SEARCH_PATH=!FEAT_INSTALL_ROOT!"
 goto :eof
 
 
