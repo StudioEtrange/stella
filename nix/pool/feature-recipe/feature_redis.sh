278fd3c9e049acddfc1ed5331ed8013bca86c947
StudioEtrange <sboucault@gmail.com>
2018-02-19 03:55:24 +0100

    nix : redis

--- nix/pool/feature-recipe/feature_redis.sh
+++ nix/pool/feature-recipe/feature_redis.sh
@@ -2,13 +2,43 @@ if [ ! "$_redis_INCLUDED_" = "1" ]; then
 _redis_INCLUDED_=1
 
 
+# TODO : openssl dependencies ?
 
 feature_redis() {
 	FEAT_NAME=redis
-	FEAT_LIST_SCHEMA="3_0_7:source"
+	FEAT_LIST_SCHEMA="4_0_8:source 3_0_7:source"
 	FEAT_DEFAULT_ARCH=
 	FEAT_DEFAULT_FLAVOUR="source"
 }
+
+
+
+
+
+feature_redis_4_0_8() {
+	FEAT_VERSION=4_0_8
+
+
+	FEAT_SOURCE_DEPENDENCIES=
+	FEAT_BINARY_DEPENDENCIES=
+
+	FEAT_SOURCE_URL=http://download.redis.io/releases/redis-4.0.8.tar.gz
+	FEAT_SOURCE_URL_FILENAME=redis-4.0.8.tar.gz
+	FEAT_SOURCE_URL_PROTOCOL=HTTP_ZIP
+
+	FEAT_BINARY_URL=
+	FEAT_BINARY_URL_FILENAME=
+	FEAT_BINARY_URL_PROTOCOL=
+
+	FEAT_SOURCE_CALLBACK=
+	FEAT_BINARY_CALLBACK=
+	FEAT_ENV_CALLBACK=
+
+	FEAT_INSTALL_TEST="$FEAT_INSTALL_ROOT"/bin/redis-server
+	FEAT_SEARCH_PATH="$FEAT_INSTALL_ROOT"/bin
+
+}
+
 
 feature_redis_3_0_7() {
 	FEAT_VERSION=3_0_7
