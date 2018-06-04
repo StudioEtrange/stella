328287a8a97fa989ea41ce23227d92f0212860eb
StudioEtrange <sboucault@gmail.com>
2018-06-05 01:51:50 +0200

    nix: reg update link

--- nix/pool/feature-recipe/feature_reg.sh
+++ nix/pool/feature-recipe/feature_reg.sh
@@ -1,7 +1,7 @@
 if [ ! "$_reg_INCLUDED_" = "1" ]; then
 _reg_INCLUDED_=1
 
-#https://github.com/jessfraz/reg
+#https://github.com/genuinetools/reg
 
 feature_reg() {
 	FEAT_NAME=reg
@@ -10,7 +10,7 @@ feature_reg() {
 	FEAT_DEFAULT_FLAVOUR="binary"
 
 	FEAT_DESC="docker registry client"
-	FEAT_LINK="https://github.com/jessfraz/reg"
+	FEAT_LINK="https://github.com/genuinetools/reg"
 }
 
 
@@ -23,18 +23,18 @@ feature_reg_0_13_0() {
 
 	# Properties for BINARY flavour
 	if [ "$STELLA_CURRENT_PLATFORM" = "darwin" ]; then
-		FEAT_BINARY_URL_x86="https://github.com/jessfraz/reg/releases/download/v0.13.0/reg-darwin-386"
+		FEAT_BINARY_URL_x86="https://github.com/genuinetools/reg/releases/download/v0.13.0/reg-darwin-386"
 		FEAT_BINARY_URL_FILENAME_x86="reg-darwin-386-$FEAT_VERSION"
 		FEAT_BINARY_URL_PROTOCOL_x86="HTTP"
-		FEAT_BINARY_URL_x64="https://github.com/jessfraz/reg/releases/download/v0.13.0/reg-darwin-amd64"
+		FEAT_BINARY_URL_x64="https://github.com/genuinetools/reg/releases/download/v0.13.0/reg-darwin-amd64"
 		FEAT_BINARY_URL_FILENAME_x64="reg-darwin-amd64-$FEAT_VERSION"
 		FEAT_BINARY_URL_PROTOCOL_x64="HTTP"
 	fi
 	if [ "$STELLA_CURRENT_PLATFORM" = "linux" ]; then
-		FEAT_BINARY_URL_x86="https://github.com/jessfraz/reg/releases/download/v0.13.0/reg-linux-386"
+		FEAT_BINARY_URL_x86="https://github.com/genuinetools/reg/releases/download/v0.13.0/reg-linux-386"
 		FEAT_BINARY_URL_FILENAME_x86="reg-linux-386-$FEAT_VERSION"
 		FEAT_BINARY_URL_PROTOCOL_x86="HTTP"
-		FEAT_BINARY_URL_x64="https://github.com/jessfraz/reg/releases/download/v0.13.0/reg-linux-amd64"
+		FEAT_BINARY_URL_x64="https://github.com/genuinetools/reg/releases/download/v0.13.0/reg-linux-amd64"
 		FEAT_BINARY_URL_FILENAME_x64="reg-linux-amd64-$FEAT_VERSION"
 		FEAT_BINARY_URL_PROTOCOL_x64="HTTP"
 	fi
@@ -62,18 +62,18 @@ feature_reg_0_9_0() {
 
 	# Properties for BINARY flavour
 	if [ "$STELLA_CURRENT_PLATFORM" = "darwin" ]; then
-		FEAT_BINARY_URL_x86="https://github.com/jessfraz/reg/releases/download/v0.9.0/reg-darwin-386"
+		FEAT_BINARY_URL_x86="https://github.com/genuinetools/reg/releases/download/v0.9.0/reg-darwin-386"
 		FEAT_BINARY_URL_FILENAME_x86="reg-darwin-386-$FEAT_VERSION"
 		FEAT_BINARY_URL_PROTOCOL_x86="HTTP"
-		FEAT_BINARY_URL_x64="https://github.com/jessfraz/reg/releases/download/v0.9.0/reg-darwin-amd64"
+		FEAT_BINARY_URL_x64="https://github.com/genuinetools/reg/releases/download/v0.9.0/reg-darwin-amd64"
 		FEAT_BINARY_URL_FILENAME_x64="reg-darwin-amd64-$FEAT_VERSION"
 		FEAT_BINARY_URL_PROTOCOL_x64="HTTP"
 	fi
 	if [ "$STELLA_CURRENT_PLATFORM" = "linux" ]; then
-		FEAT_BINARY_URL_x86="https://github.com/jessfraz/reg/releases/download/v0.9.0/reg-linux-386"
+		FEAT_BINARY_URL_x86="https://github.com/genuinetools/reg/releases/download/v0.9.0/reg-linux-386"
 		FEAT_BINARY_URL_FILENAME_x86="reg-linux-386-$FEAT_VERSION"
 		FEAT_BINARY_URL_PROTOCOL_x86="HTTP"
-		FEAT_BINARY_URL_x64="https://github.com/jessfraz/reg/releases/download/v0.9.0/reg-linux-amd64"
+		FEAT_BINARY_URL_x64="https://github.com/genuinetools/reg/releases/download/v0.9.0/reg-linux-amd64"
 		FEAT_BINARY_URL_FILENAME_x64="reg-linux-amd64-$FEAT_VERSION"
 		FEAT_BINARY_URL_PROTOCOL_x64="HTTP"
 	fi
