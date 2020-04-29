5a9b758b1c2da17615579761d45dab17685722d8
StudioEtrange <nomorgan@gmail.com>
2020-04-29 05:01:21 +0200

    nix : update feature yq

--- nix/pool/feature-recipe/feature_yq.sh
+++ nix/pool/feature-recipe/feature_yq.sh
@@ -3,7 +3,7 @@ _yq_INCLUDED_=1
 
 feature_yq() {
 	FEAT_NAME="yq"
-	FEAT_LIST_SCHEMA="3_1_2@x64:binary 3_1_2@x86:binary"
+	FEAT_LIST_SCHEMA="3_3_0@x64:binary 3_1_2@x64:binary 3_1_2@x86:binary"
 	FEAT_DEFAULT_ARCH="x64"
 	FEAT_DEFAULT_FLAVOUR="binary"
 
@@ -11,6 +11,33 @@ feature_yq() {
 	FEAT_LINK="https://mikefarah.gitbook.io/yq/ https://github.com/mikefarah/yq"
 }
 
+
+
+feature_yq_3_3_0() {
+	FEAT_VERSION="3_3_0"
+
+	if [ "${STELLA_CURRENT_PLATFORM}" = "linux" ]; then
+		FEAT_BINARY_URL_x86="https://github.com/mikefarah/yq/releases/download/3.3.0/yq_linux_386"
+		FEAT_BINARY_URL_FILENAME_x86="yq_linux_386_${FEAT_VERSION}"
+		FEAT_BINARY_URL_PROTOCOL_x86="HTTP"
+		FEAT_BINARY_URL_x64="https://github.com/mikefarah/yq/releases/download/3.3.0/yq_linux_amd64"
+		FEAT_BINARY_URL_FILENAME_x64="yq_linux_amd64_${FEAT_VERSION}"
+		FEAT_BINARY_URL_PROTOCOL_x64="HTTP"
+
+	fi
+	if [ "${STELLA_CURRENT_PLATFORM}" = "darwin" ]; then
+		FEAT_BINARY_URL_x86="https://github.com/mikefarah/yq/releases/download/3.3.0/yq_darwin_386"
+		FEAT_BINARY_URL_FILENAME_x86="yq_darwin_386_${FEAT_VERSION}"
+		FEAT_BINARY_URL_PROTOCOL_x86="HTTP"
+		FEAT_BINARY_URL_x64="https://github.com/mikefarah/yq/releases/download/3.3.0/yq_darwin_amd64"
+		FEAT_BINARY_URL_FILENAME_x64="yq_darwin_amd64_${FEAT_VERSION}"
+		FEAT_BINARY_URL_PROTOCOL_x64="HTTP"
+	fi
+
+	FEAT_INSTALL_TEST="${FEAT_INSTALL_ROOT}"/yq
+	FEAT_SEARCH_PATH="${FEAT_INSTALL_ROOT}"
+
+}
 
 feature_yq_3_1_2() {
 	FEAT_VERSION="3_1_2"
