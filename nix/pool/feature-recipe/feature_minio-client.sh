f4a324e6a324cde7f8cad10b507ad2cf04b6b4a2
StudioEtrange <nomorgan@gmail.com>
2020-07-07 00:26:55 +0200

    nix : update minio-client recipe

--- nix/pool/feature-recipe/feature_minio-client.sh
+++ nix/pool/feature-recipe/feature_minio-client.sh
@@ -5,8 +5,7 @@ _MINIOCLIENT_INCLUDED_=1
 feature_minio-client() {
 
 	FEAT_NAME=minio-client
-	[ "$STELLA_CURRENT_PLATFORM" = "darwin" ] && FEAT_LIST_SCHEMA="latest@x64:binary"
-	[ "$STELLA_CURRENT_PLATFORM" = "linux" ] && FEAT_LIST_SCHEMA="latest@x64:binary latest@x86:binary"
+	FEAT_LIST_SCHEMA="latest@x64:binary RELEASE_20200626@x64:binary"
 	FEAT_DEFAULT_ARCH=x64
 	FEAT_DEFAULT_FLAVOUR="binary"
 }
@@ -31,9 +30,6 @@ feature_minio-client_latest() {
 		FEAT_BINARY_URL_x64=https://dl.minio.io/client/mc/release/linux-amd64/mc
 		FEAT_BINARY_URL_FILENAME_x64="mc_${FEAT_VERSION}_linux_amd64_$(date +%s)"
 		FEAT_BINARY_URL_PROTOCOL_x64=HTTP
-		FEAT_BINARY_URL_x86=https://dl.minio.io/client/mc/release/linux-386/mc
-		FEAT_BINARY_URL_FILENAME_x86="mc_${FEAT_VERSION}_linux_x86_$(date +%s)"
-		FEAT_BINARY_URL_PROTOCOL_x86=HTTP
 	fi
 
 	FEAT_SOURCE_CALLBACK=
@@ -43,6 +39,39 @@ feature_minio-client_latest() {
 	FEAT_INSTALL_TEST="$FEAT_INSTALL_ROOT"/mc
 	FEAT_SEARCH_PATH="$FEAT_INSTALL_ROOT"
 }
+
+
+
+feature_minio-client_RELEASE_20200626() {
+	FEAT_VERSION="RELEASE_20200626"
+
+	FEAT_SOURCE_DEPENDENCIES=
+	FEAT_BINARY_DEPENDENCIES=
+
+	FEAT_SOURCE_URL=
+	FEAT_SOURCE_URL_FILENAME=
+	FEAT_SOURCE_URL_PROTOCOL=
+
+	# NOTE we use $(date +%s) as a timestamp to invalidate cached version, because this is the latest
+	if [ "$STELLA_CURRENT_PLATFORM" = "darwin" ]; then
+		FEAT_BINARY_URL_x64=https://dl.minio.io/client/mc/release/darwin-amd64/archive/mc.RELEASE.2020-06-26T19-56-55Z
+		FEAT_BINARY_URL_FILENAME_x64="mc_${FEAT_VERSION}_darwin"
+		FEAT_BINARY_URL_PROTOCOL_x64=HTTP
+	fi
+	if [ "$STELLA_CURRENT_PLATFORM" = "linux" ]; then
+		FEAT_BINARY_URL_x64=https://dl.minio.io/client/mc/release/linux-amd64/archive/mc.RELEASE.2020-06-26T19-56-55Z
+		FEAT_BINARY_URL_FILENAME_x64="mc_${FEAT_VERSION}_linux_amd64"
+		FEAT_BINARY_URL_PROTOCOL_x64=HTTP
+	fi
+
+	FEAT_SOURCE_CALLBACK=
+	FEAT_BINARY_CALLBACK=
+	FEAT_ENV_CALLBACK=
+
+	FEAT_INSTALL_TEST="$FEAT_INSTALL_ROOT"/mc
+	FEAT_SEARCH_PATH="$FEAT_INSTALL_ROOT"
+}
+
 
 # -----------------------------------------
 feature_minio-client_install_binary() {
