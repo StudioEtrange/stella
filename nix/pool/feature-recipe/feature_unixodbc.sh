a56ddd850faa2c1cb924d68172cbe994a255d26f
StudioEtrange <nomorgan@gmail.com>
2021-01-28 03:03:44 +0100

    nix: update unixodbc recipe

--- nix/pool/feature-recipe/feature_unixodbc.sh
+++ nix/pool/feature-recipe/feature_unixodbc.sh
@@ -6,11 +6,33 @@ _unixodbc_INCLUDED_=1
 
 feature_unixodbc() {
 	FEAT_NAME=unixodbc
-	FEAT_LIST_SCHEMA="2_3_7:source 2_3_4:source"
+	FEAT_LIST_SCHEMA="2_3_9:source 2_3_8:source 2_3_7:source 2_3_4:source"
 	FEAT_DEFAULT_FLAVOUR="source"
 	
 	FEAT_DESC="The unixODBC Project goals are to develop and promote unixODBC to be the definitive standard for ODBC on non MS Windows platforms. ODBC is an open specification for providing application developers with a predictable API with which to access Data Sources. Data Sources include SQL Servers and any Data Source with an ODBC Driver."
 	FEAT_LINK="http://www.unixodbc.org/"
+}
+
+feature_unixodbc_2_3_9() {
+	FEAT_VERSION=2_3_9
+
+	FEAT_SOURCE_URL="http://www.unixodbc.org/unixODBC-2.3.9.tar.gz"
+	FEAT_SOURCE_URL_FILENAME="unixODBC-2.3.9.tar.gz"
+	FEAT_SOURCE_URL_PROTOCOL=HTTP_ZIP
+
+	FEAT_INSTALL_TEST="$FEAT_INSTALL_ROOT"/bin/odbc_config
+	FEAT_SEARCH_PATH="$FEAT_INSTALL_ROOT"/bin
+}
+
+feature_unixodbc_2_3_8() {
+	FEAT_VERSION=2_3_8
+
+	FEAT_SOURCE_URL="http://www.unixodbc.org/unixODBC-2.3.8.tar.gz"
+	FEAT_SOURCE_URL_FILENAME="unixODBC-2.3.7.tar.gz"
+	FEAT_SOURCE_URL_PROTOCOL=HTTP_ZIP
+
+	FEAT_INSTALL_TEST="$FEAT_INSTALL_ROOT"/bin/odbc_config
+	FEAT_SEARCH_PATH="$FEAT_INSTALL_ROOT"/bin
 }
 
 feature_unixodbc_2_3_7() {
