b02513fb4cabd9f6949c230c4cae12e866142ff2
StudioEtrange <nomorgan@gmail.com>
2021-06-07 04:08:04 +0200

    nix : update recipe openssl

--- nix/pool/feature-recipe/feature_openssl.sh
+++ nix/pool/feature-recipe/feature_openssl.sh
@@ -57,112 +57,123 @@ _OPENSSL_INCLUDED_=1
 # 1_0_2o@x64:binary 1_0_2o@x86:binary
 
 feature_openssl() {
-	FEAT_NAME=openssl
-	FEAT_LIST_SCHEMA="1_0_2k:source 1_0_2d:source"
-	FEAT_DEFAULT_ARCH=x64
-	FEAT_DEFAULT_FLAVOUR="binary"
+	FEAT_NAME="openssl"
+	FEAT_LIST_SCHEMA="1_1_1k:source 1_0_2k:source 1_0_2d:source"
+	FEAT_DEFAULT_ARCH=""
+	FEAT_DEFAULT_FLAVOUR="source"
+
+	FEAT_DESC="OpenSSL is a robust, commercial-grade, and full-featured toolkit for the Transport Layer Security (TLS) and Secure Sockets Layer (SSL) protocols."
+	FEAT_LINK="https://www.openssl.org/"
 }
 
 
 
-feature_openssl_1_0_2o() {
-	FEAT_VERSION=1_0_2o
 
-	FEAT_SOURCE_DEPENDENCIES=
+
+
+feature_openssl_1_1_1k() {
+	FEAT_VERSION="1_1_1k"
+
+	FEAT_SOURCE_DEPENDENCIES="zlib#^1_2"
 	FEAT_BINARY_DEPENDENCIES=
 
-	FEAT_SOURCE_URL=
-	FEAT_SOURCE_URL_FILENAME=
-	FEAT_SOURCE_URL_PROTOCOL=
+	FEAT_SOURCE_URL="https://www.openssl.org/source/openssl-1.1.1k.tar.gz"
+	FEAT_SOURCE_URL_FILENAME="openssl-1.1.1k.tar.gz"
+	FEAT_SOURCE_URL_PROTOCOL="HTTP_ZIP"
 
-	if [ "$STELLA_CURRENT_PLATFORM" = "linux" ]; then
-		COMPIL_FRONTEND_VERSION="$(__gcc_version)"
-		FEAT_BINARY_URL_x86="http://download.oracle.com/otn-pub/java/jdk/8u152-b16/aa0333dd3019491ca4f6ddbe78cdb6d0/jdk-8u152-linux-i586.tar.gz"
-		FEAT_BINARY_URL_FILENAME_x86=jdk-8u152-linux-i586.tar.gz
-		FEAT_BINARY_URL_PROTOCOL_x86=HTTP_ZIP
+	FEAT_BINARY_URL=
+	FEAT_BINARY_URL_FILENAME=
+	FEAT_BINARY_URL_PROTOCOL=
 
-		FEAT_BINARY_URL_x64="http://download.oracle.com/otn-pub/java/jdk/8u152-b16/aa0333dd3019491ca4f6ddbe78cdb6d0/jdk-8u152-linux-x64.tar.gz"
-		FEAT_BINARY_URL_FILENAME_x64=jdk-8u152-linux-x64.tar.gz
-		FEAT_BINARY_URL_PROTOCOL_x64=HTTP_ZIP
-	fi
-
-	if [ "$STELLA_CURRENT_PLATFORM" = "darwin" ]; then
-		COMPIL_FRONTEND_VERSION="$(__clang_version)"
-
-		# build_type=Release AND arch=x86_64 AND os=Macos AND compiler=apple-clang
-		#					compiler.version=7.3 AND shared=True			ce60e8abcb35e7bdbb633792511ac6240e2a2ceb
-		#					compiler.version=7.3 AND shared=False			b66d1d7c33354c2d4701c7f4d67b40395e741a86
-		#					compiler.version=8.1 AND shared=True			4dc21962ebc63953d4b0879c71588b3b46212e03
-		#					compiler.version=8.1 AND shared=False			0197c20e330042c026560da838f5b4c4bf094b8a
-		#					compiler.version=9.0 AND shared=True			5e9b4170b1252b259e07e6ebd53940386a1c30e6
-		#					compiler.version=9.0 AND shared=False			227fb0ea22f4797212e72ba94ea89c7b3fbc2a0c
-		#					compiler.version=9.1 AND shared=True			b58c275d0c193bfbdf9cf9875a4cd2b8048dea19
-		#					compiler.version=9.1 AND shared=False			b3e8c6b6e5f8456a00d1a77a6a5a1aeb06b2ad48
-	fi
-
-	FEAT_SOURCE_CALLBACK=
+	FEAT_SOURCE_CALLBACK="feature_openssl_link feature_openssl_options_1_1_1"
 	FEAT_BINARY_CALLBACK=
 	FEAT_ENV_CALLBACK=
 
-	FEAT_INSTALL_TEST="$FEAT_INSTALL_ROOT"/bin/openssl
-	FEAT_SEARCH_PATH="$FEAT_INSTALL_ROOT"/bin
+	FEAT_INSTALL_TEST="$FEAT_INSTALL_ROOT/bin/openssl"
+	FEAT_SEARCH_PATH="$FEAT_INSTALL_ROOT/bin"
 
 }
 
 
 feature_openssl_1_0_2k() {
-	FEAT_VERSION=1_0_2k
+	FEAT_VERSION="1_0_2k"
 
 	FEAT_SOURCE_DEPENDENCIES="zlib#^1_2"
 	FEAT_BINARY_DEPENDENCIES=
 
-	FEAT_SOURCE_URL=https://www.openssl.org/source/openssl-1.0.2k.tar.gz
-	FEAT_SOURCE_URL_FILENAME=openssl-1.0.2k.tar.gz
-	FEAT_SOURCE_URL_PROTOCOL=HTTP_ZIP
+	FEAT_SOURCE_URL="https://www.openssl.org/source/openssl-1.0.2k.tar.gz"
+	FEAT_SOURCE_URL_FILENAME="openssl-1.0.2k.tar.gz"
+	FEAT_SOURCE_URL_PROTOCOL="HTTP_ZIP"
 
 	FEAT_BINARY_URL=
 	FEAT_BINARY_URL_FILENAME=
 	FEAT_BINARY_URL_PROTOCOL=
 
-	FEAT_SOURCE_CALLBACK=feature_openssl_link
+	FEAT_SOURCE_CALLBACK="feature_openssl_link feature_openssl_options_1_0_2"
 	FEAT_BINARY_CALLBACK=
 	FEAT_ENV_CALLBACK=
 
-	FEAT_INSTALL_TEST="$FEAT_INSTALL_ROOT"/bin/openssl
-	FEAT_SEARCH_PATH="$FEAT_INSTALL_ROOT"/bin
+	FEAT_INSTALL_TEST="$FEAT_INSTALL_ROOT/bin/openssl"
+	FEAT_SEARCH_PATH="$FEAT_INSTALL_ROOT/bin"
 
 }
 
 
 feature_openssl_1_0_2d() {
-	FEAT_VERSION=1_0_2d
+	FEAT_VERSION="1_0_2d"
 
 	FEAT_SOURCE_DEPENDENCIES="zlib#^1_2"
 	FEAT_BINARY_DEPENDENCIES=
 
-	FEAT_SOURCE_URL=https://www.openssl.org/source/openssl-1.0.2d.tar.gz
-	FEAT_SOURCE_URL_FILENAME=openssl-1.0.2d.tar.gz
-	FEAT_SOURCE_URL_PROTOCOL=HTTP_ZIP
+	FEAT_SOURCE_URL="https://www.openssl.org/source/openssl-1.0.2d.tar.gz"
+	FEAT_SOURCE_URL_FILENAME="openssl-1.0.2d.tar.gz"
+	FEAT_SOURCE_URL_PROTOCOL="HTTP_ZIP"
 
 	FEAT_BINARY_URL=
 	FEAT_BINARY_URL_FILENAME=
 	FEAT_BINARY_URL_PROTOCOL=
 
-	FEAT_SOURCE_CALLBACK=feature_openssl_link
+	FEAT_SOURCE_CALLBACK="feature_openssl_link feature_openssl_options_1_0_2"
 	FEAT_BINARY_CALLBACK=
 	FEAT_ENV_CALLBACK=
 
-	FEAT_INSTALL_TEST="$FEAT_INSTALL_ROOT"/bin/openssl
-	FEAT_SEARCH_PATH="$FEAT_INSTALL_ROOT"/bin
+	FEAT_INSTALL_TEST="$FEAT_INSTALL_ROOT/bin/openssl"
+	FEAT_SEARCH_PATH="$FEAT_INSTALL_ROOT/bin"
 
 }
 
 feature_openssl_link() {
 	# zlib dependencies
-	__link_feature_library "zlib#^1_2" "LIBS_NAME z GET_FLAGS _zlib FORCE_DYNAMIC NO_SET_FLAGS"
+	__link_feature_library "zlib" "LIBS_NAME z GET_FLAGS _zlib FORCE_DYNAMIC NO_SET_FLAGS"
 }
 
 
+feature_openssl_options_1_0_2() {
+	if [ "$STELLA_CURRENT_PLATFORM" = "darwin" ]; then
+		OPENSSL_OPT="shared no-idea no-mdc2 no-rc5 enable-ssl2 enable-tlsext enable-cms"
+		if [ "$ARCH" = "x64" ]; then
+			OPENSSL_OPT="$OPENSSL_OPT enable-ec_nistp_64_gcc_128"
+		fi
+	fi
+
+	if [ "$STELLA_CURRENT_PLATFORM" = "linux" ]; then
+		OPENSSL_OPT="shared no-idea no-mdc2 no-rc5 enable-ssl2 enable-tlsext enable-cms enable-krb5"	
+	fi
+
+}
+
+feature_openssl_options_1_1_1() {
+	if [ "$STELLA_CURRENT_PLATFORM" = "darwin" ]; then
+		OPENSSL_OPT="shared no-idea no-mdc2 no-rc5 enable-ssl2 enable-cms"
+		if [ "$ARCH" = "x64" ]; then
+			OPENSSL_OPT="$OPENSSL_OPT enable-ec_nistp_64_gcc_128"
+		fi
+	fi
+
+	if [ "$STELLA_CURRENT_PLATFORM" = "linux" ]; then
+		OPENSSL_OPT="shared no-idea no-mdc2 no-rc5 enable-ssl2 enable-cms"	
+	fi
+}
 
 feature_openssl_install_source() {
 	INSTALL_DIR="$FEAT_INSTALL_ROOT"
@@ -179,26 +190,20 @@ feature_openssl_install_source() {
 	[ "$ARCH" = "" ] && ARCH="x64"
 
 	if [ "$STELLA_CURRENT_PLATFORM" = "darwin" ]; then
-		OPENSSL_OPT="shared no-idea no-mdc2 no-rc5 enable-ssl2 enable-tlsext enable-cms"
-
 		[ "$ARCH" = "x86" ] && OPENSSL_PLATFORM="darwin-i386-cc"
 		if [ "$ARCH" = "x64" ]; then
 			OPENSSL_PLATFORM="darwin64-x86_64-cc"
-			OPENSSL_OPT="$OPENSSL_OPT enable-ec_nistp_64_gcc_128"
 		fi
 	fi
 
 	if [ "$STELLA_CURRENT_PLATFORM" = "linux" ]; then
-		OPENSSL_OPT="shared no-idea no-mdc2 no-rc5 enable-ssl2 enable-tlsext enable-cms enable-krb5"
-
-		[ "$ARCH" = "x86" ] && OPENSSL_PLATFORM=linux-generic32
-		[ "$ARCH" = "x64" ] && OPENSSL_PLATFORM=linux-x86_64
+		[ "$ARCH" = "x86" ] && OPENSSL_PLATFORM="linux-generic32"
+		[ "$ARCH" = "x64" ] && OPENSSL_PLATFORM="linux-x86_64"
 	fi
 
 	__feature_callback
 
 	__start_manual_build "openssl" "$SRC_DIR" "$INSTALL_DIR"
-	#__prepare_build "$INSTALL_DIR" "$SRC_DIR" "$SRC_DIR"
 
 	cd "$SRC_DIR"
 	# configure --------------------------------
@@ -226,7 +231,7 @@ feature_openssl_install_source() {
 	cd "$INSTALL_DIR"
 	rm -Rf "$SRC_DIR"
 
-	__inspect_and_fix_build "$INSTALL_DIR" "EXCLUDE_FILTER /share/man/"
+	__inspect_and_fix_build "$INSTALL_DIR" "EXCLUDE_FILTER /share/"
 }
 
 
