6930c7ad7ddce35258a6cebaa55f69846113fd21
StudioEtrange <nomorgan@gmail.com>
2021-10-26 18:45:56 +0200

    nix : fix recipe nuctl

--- nix/pool/feature-recipe/feature_nuctl.sh
+++ nix/pool/feature-recipe/feature_nuctl.sh
@@ -25,19 +25,19 @@ feature_nuctl_1_6_27() {
 	if [ "$STELLA_CURRENT_PLATFORM" = "darwin" ]; then
 		FEAT_BINARY_URL_x64="https://github.com/nuclio/nuclio/releases/download/1.6.27/nuctl-1.6.27-darwin-amd64"
 		FEAT_BINARY_URL_FILENAME_x64="nuctl-1.6.27-darwin-amd64"
-		FEAT_BINARY_URL_PROTOCOL_x64="HTTP_ZIP"
+		FEAT_BINARY_URL_PROTOCOL_x64="HTTP"
 	fi
 	if [ "$STELLA_CURRENT_PLATFORM" = "linux" ]; then
 		FEAT_BINARY_URL_x64="https://github.com/nuclio/nuclio/releases/download/1.6.27/nuctl-1.6.27-linux-amd64"
 		FEAT_BINARY_URL_FILENAME_x64="nuctl-1.6.27-linux-amd64"
-		FEAT_BINARY_URL_PROTOCOL_x64="HTTP_ZIP"
+		FEAT_BINARY_URL_PROTOCOL_x64="HTTP"
 	fi
 
 
 	# List of files to test if feature is installed
-	FEAT_INSTALL_TEST="$FEAT_INSTALL_ROOT/bin/nuctl"
+	FEAT_INSTALL_TEST="${FEAT_INSTALL_ROOT}/nuctl"
 	# PATH to add to system PATH
-	FEAT_SEARCH_PATH="$FEAT_INSTALL_ROOT/bin"
+	FEAT_SEARCH_PATH="${FEAT_INSTALL_ROOT}"
 }
 
 
@@ -46,10 +46,10 @@ feature_nuctl_1_6_27() {
 
 feature_nuctl_install_binary() {
 
-	__get_resource "$FEAT_NAME" "$FEAT_BINARY_URL" "$FEAT_BINARY_URL_PROTOCOL" "$FEAT_INSTALL_ROOT" "DEST_ERASE STRIP FORCE_NAME $FEAT_BINARY_URL_FILENAME"
+	__get_resource "$FEAT_NAME" "$FEAT_BINARY_URL" "$FEAT_BINARY_URL_PROTOCOL" "$FEAT_INSTALL_ROOT" "DEST_ERASE FORCE_NAME $FEAT_BINARY_URL_FILENAME"
 
-	__feature_callback
-
+	mv "${FEAT_INSTALL_ROOT}/${FEAT_BINARY_URL_FILENAME}" "${FEAT_INSTALL_ROOT}/nuctl"
+	chmod +x "${FEAT_INSTALL_ROOT}/nuctl"
 }
 
 
