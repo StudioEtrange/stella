d1dc72cc128cddc6e17e25f98148c07c94483fb1
StudioEtrange <nomorgan@gmail.com>
2022-07-14 16:09:49 +0200

    nix : add feature ngrok

--- nix/pool/feature-recipe/feature_ngrok.sh
+++ nix/pool/feature-recipe/feature_ngrok.sh
@@ -4,51 +4,66 @@ _NGROK_INCLUDED_=1
 
 feature_ngrok() {
 	FEAT_NAME=ngrok
-	FEAT_LIST_SCHEMA="stable@x86:binary stable@x64:binary"
-	FEAT_DEFAULT_ARCH=x64
+	# version stable is considered in order the one to choose by default
+	FEAT_LIST_SCHEMA="stable@x86:binary\macos stable@x64:binary 2_3_40@x86:binary\macos 2_3_40@x64:binary"
+	FEAT_DEFAULT_ARCH="x64"
 	FEAT_DEFAULT_FLAVOUR="binary"
+
+	FEAT_DESC="ngrok is the programmable network edge that adds connectivity, security, and observability to your apps with no code changes"
+	FEAT_LINK="https://ngrok.com/ https://dl.equinox.io/ngrok/ngrok/stable"
 }
 
 feature_ngrok_stable() {
-	FEAT_VERSION=stable
+	FEAT_VERSION="stable"
 
-	FEAT_SOURCE_DEPENDENCIES=
-	FEAT_BINARY_DEPENDENCIES=
-
-	FEAT_SOURCE_URL=
-	FEAT_SOURCE_URL_FILENAME=
 
 	if [ "$STELLA_CURRENT_PLATFORM" = "darwin" ]; then
-		FEAT_BINARY_URL_x64="https://api.equinox.io/1/Applications/ap_pJSFC5wQYkAyI0FIVwKYs9h1hW/Updates/Asset/ngrok.zip?channel=stable&os=darwin&arch=amd64"
-		FEAT_BINARY_URL_FILENAME_x64=ngrok_darwin_amd64.zip
-		FEAT_BINARY_URL_PROTOCOL_x64=HTTP_ZIP
-		FEAT_BINARY_URL_x86="https://api.equinox.io/1/Applications/ap_pJSFC5wQYkAyI0FIVwKYs9h1hW/Updates/Asset/ngrok.zip?channel=stable&os=darwin&arch=386"
-		FEAT_BINARY_URL_FILENAME_x86=ngrok_darwin_386.zip
-		FEAT_BINARY_URL_PROTOCOL_x86=HTTP_ZIP
+		FEAT_BINARY_URL_x64="https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-darwin-amd64.zip"
+		FEAT_BINARY_URL_FILENAME_x64="ngrok-stable-darwin-amd64.zip"
+		FEAT_BINARY_URL_PROTOCOL_x64="HTTP_ZIP"
 	fi
 
-	if [ "$STELLA_CURRENT_PLATFORM" = "linux" ]; then
-		FEAT_BINARY_URL_x64="https://api.equinox.io/1/Applications/ap_pJSFC5wQYkAyI0FIVwKYs9h1hW/Updates/Asset/ngrok.zip?channel=stable&os=linux&arch=amd64"
-		FEAT_BINARY_URL_FILENAME_x64=ngrok_linux_amd64.zip
-		FEAT_BINARY_URL_PROTOCOL_x64=HTTP_ZIP
-		FEAT_BINARY_URL_x86="https://api.equinox.io/1/Applications/ap_pJSFC5wQYkAyI0FIVwKYs9h1hW/Updates/Asset/ngrok.zip?channel=stable&os=linux&arch=386"
-		FEAT_BINARY_URL_FILENAME_x86=ngrok_linux_386.zip
-		FEAT_BINARY_URL_PROTOCOL_x86=HTTP_ZIP
+	if [ "${STELLA_CURRENT_PLATFORM}" = "linux" ]; then
+		FEAT_BINARY_URL_x64="https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.tgz"
+		FEAT_BINARY_URL_FILENAME_x64="ngrok-stable-linux-amd64.tgz"
+		FEAT_BINARY_URL_PROTOCOL_x64="HTTP_ZIP"
+		FEAT_BINARY_URL_x86="https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-386.tgz"
+		FEAT_BINARY_URL_FILENAME_x86="ngrok-stable-linux-386.tgz"
+		FEAT_BINARY_URL_PROTOCOL_x86="HTTP_ZIP"
 	fi
 
-	FEAT_SOURCE_CALLBACK=
-	FEAT_BINARY_CALLBACK=
-	FEAT_ENV_CALLBACK=
-
-
-	FEAT_INSTALL_TEST="$FEAT_INSTALL_ROOT"/ngrok
-	FEAT_SEARCH_PATH="$FEAT_INSTALL_ROOT"
+	FEAT_INSTALL_TEST="${FEAT_INSTALL_ROOT}/ngrok"
+	FEAT_SEARCH_PATH="${FEAT_INSTALL_ROOT}"
 }
 
 
+
+feature_ngrok_2_3_40() {
+	FEAT_VERSION="2_3_40"
+
+
+	if [ "$STELLA_CURRENT_PLATFORM" = "darwin" ]; then
+		FEAT_BINARY_URL_x64="https://bin.equinox.io/a/mL7fbqPrAXM/ngrok-2.3.40-darwin-amd64.tar.gz"
+		FEAT_BINARY_URL_FILENAME_x64="ngrok-2.3.40-darwin-amd64.tar.gz"
+		FEAT_BINARY_URL_PROTOCOL_x64="HTTP_ZIP"
+	fi
+
+	if [ "${STELLA_CURRENT_PLATFORM}" = "linux" ]; then
+		FEAT_BINARY_URL_x64="https://bin.equinox.io/a/6sdfF9NmmRW/ngrok-2.3.40-linux-amd64.tar.gz"
+		FEAT_BINARY_URL_FILENAME_x64="ngrok-2.3.40-linux-amd64.tar.gz"
+		FEAT_BINARY_URL_PROTOCOL_x64="HTTP_ZIP"
+		FEAT_BINARY_URL_x86="https://bin.equinox.io/a/gsR7QMLiGHR/ngrok-2.3.40-linux-386.tar.gz"
+		FEAT_BINARY_URL_FILENAME_x86="ngrok-2.3.40-linux-386.tar.gz"
+		FEAT_BINARY_URL_PROTOCOL_x86="HTTP_ZIP"
+	fi
+
+	FEAT_INSTALL_TEST="${FEAT_INSTALL_ROOT}/ngrok"
+	FEAT_SEARCH_PATH="${FEAT_INSTALL_ROOT}"
+}
+
 # -----------------------------------------
 feature_ngrok_install_binary() {
-	__get_resource "$FEAT_NAME" "$FEAT_BINARY_URL" "$FEAT_BINARY_URL_PROTOCOL" "$FEAT_INSTALL_ROOT" "DEST_ERASE STRIP FORCE_NAME $FEAT_BINARY_URL_FILENAME"
+	__get_resource "${FEAT_NAME}" "${FEAT_BINARY_URL}" "${FEAT_BINARY_URL_PROTOCOL}" "${FEAT_INSTALL_ROOT}" "DEST_ERASE  FORCE_NAME ${FEAT_BINARY_URL_FILENAME}"
 
 
 }
