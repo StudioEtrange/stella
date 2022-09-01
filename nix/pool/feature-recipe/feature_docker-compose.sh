0df623893b1c99dc655d68cd2b786168693f4b79
StudioEtrange <nomorgan@gmail.com>
2022-09-02 01:00:20 +0200

    nix : recipe docker compose update

--- nix/pool/feature-recipe/feature_docker-compose.sh
+++ nix/pool/feature-recipe/feature_docker-compose.sh
@@ -4,13 +4,33 @@ _DOCKERCOMPOSE_INCLUDED_=1
 
 feature_docker-compose() {
 	FEAT_NAME="docker-compose"
-	FEAT_LIST_SCHEMA="1_29_2@x64:binary 1_25_4@x64:binary 1_23_1@x64:binary 1_21_2@x64:binary 1_18_0@x64:binary 1_11_2@x64:binary 1_9_0@x64:binary 1_8_1@x64:binary 1_1_0@x64:binary 1_3_1@x64:binary 1_5_2@x64:binary 1_7_1@x64:binary"
+	FEAT_LIST_SCHEMA="2_10_2@x64:binary 1_29_2@x64:binary 1_25_4@x64:binary 1_23_1@x64:binary 1_21_2@x64:binary 1_18_0@x64:binary 1_11_2@x64:binary 1_9_0@x64:binary 1_8_1@x64:binary 1_1_0@x64:binary 1_3_1@x64:binary 1_5_2@x64:binary 1_7_1@x64:binary"
 	FEAT_DEFAULT_ARCH="x64"
 	FEAT_DEFAULT_FLAVOUR="binary"
 
 	
 	FEAT_LINK="https://docs.docker.com/compose/"
 	FEAT_DESC="Define and run multi-container applications with Docker"
+}
+
+
+feature_docker-compose_2_10_2() {
+	FEAT_VERSION="2_10_2"
+
+	if [ "$STELLA_CURRENT_PLATFORM" = "darwin" ]; then
+		FEAT_BINARY_URL_x64="https://github.com/docker/compose/releases/download/v2.10.2/docker-compose-darwin-x86_64"
+		FEAT_BINARY_URL_FILENAME_x64="docker-compose-Darwin-x86_64-${FEAT_VERSION}"
+		FEAT_BINARY_URL_PROTOCOL_x64="HTTP"
+	fi
+	if [ "$STELLA_CURRENT_PLATFORM" = "linux" ]; then
+		FEAT_BINARY_URL_x64="https://github.com/docker/compose/releases/download/v2.10.2/docker-compose-linux-x86_64"
+		FEAT_BINARY_URL_FILENAME_x64="docker-compose-Linux-x86_64-${FEAT_VERSION}"
+		FEAT_BINARY_URL_PROTOCOL_x64="HTTP"
+	fi
+
+	FEAT_INSTALL_TEST="${FEAT_INSTALL_ROOT}/docker-compose"
+	FEAT_SEARCH_PATH="${FEAT_INSTALL_ROOT}"
+
 }
 
 
