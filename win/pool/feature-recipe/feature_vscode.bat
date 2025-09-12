88bab994230ef7f15a829c00dcff5f5862878b8b
StudioEtrange <sboucault@gmail.com>
2025-09-12 22:36:37 +0200

    win:add vscode recipe

--- win/pool/feature-recipe/feature_vscode.bat
+++ win/pool/feature-recipe/feature_vscode.bat
@@ -11,7 +11,7 @@ REM all versions download links : https://code.visualstudio.com/docs/supporting/
 :feature_vscode
 	set "FEAT_NAME=vscode"
 	REM 1_98_2 is last version that can run (remotely or not) on linux glibc 2.17
-	set "FEAT_LIST_SCHEMA=1_104_0@x64:binary 1_98_2@x64:binary"
+	set "FEAT_LIST_SCHEMA=1_104_0@x64:binary 1_98_2@x64:binary 1_96_4@x64:binary"
 	set "FEAT_DEFAULT_VERSION=1_104_0"
 	set "FEAT_DEFAULT_ARCH=x64"
 	set "FEAT_DEFAULT_FLAVOUR=binary"
@@ -61,6 +61,34 @@ goto :eof
 	set "FEAT_BINARY_URL_PROTOCOL_x86="
 	set "FEAT_BINARY_URL_x64=https://update.code.visualstudio.com/1.98.2/win32-x64-archive/stable"
 	set "FEAT_BINARY_URL_FILENAME_x64=VSCode-win32-x64-1.98.2.zip"
+	set "FEAT_BINARY_URL_PROTOCOL_x64=HTTP_ZIP"
+
+	set FEAT_SOURCE_CALLBACK=
+	set FEAT_BINARY_CALLBACK=
+	set FEAT_ENV_CALLBACK=
+
+	set "FEAT_INSTALL_TEST=!FEAT_INSTALL_ROOT!\Code.exe"
+	set "FEAT_SEARCH_PATH=!FEAT_INSTALL_ROOT!"
+
+goto :eof
+
+
+
+:feature_vscode_1_96_4
+	set "FEAT_VERSION=1_96_4"
+	
+	set FEAT_SOURCE_DEPENDENCIES=
+	set FEAT_BINARY_DEPENDENCIES=
+
+	set FEAT_SOURCE_URL=
+	set FEAT_SOURCE_URL_FILENAME=
+	set FEAT_SOURCE_URL_PROTOCOL=
+
+	set "FEAT_BINARY_URL_x86="
+	set "FEAT_BINARY_URL_FILENAME_x86="
+	set "FEAT_BINARY_URL_PROTOCOL_x86="
+	set "FEAT_BINARY_URL_x64=https://update.code.visualstudio.com/1.96.4/win32-x64-archive/stable"
+	set "FEAT_BINARY_URL_FILENAME_x64=VSCode-win32-x64-1.96.4.zip"
 	set "FEAT_BINARY_URL_PROTOCOL_x64=HTTP_ZIP"
 
 	set FEAT_SOURCE_CALLBACK=
