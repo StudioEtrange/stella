3ea9736f41e0e45d8b0e49387e2166a9b1704c55
StudioEtrange <nomorgan@gmail.com>
2025-10-17 10:45:37 +0200

    nix:update k0s and arkade recipes, add viddy recipe

--- nix/pool/feature-recipe/feature_arkade.sh
+++ nix/pool/feature-recipe/feature_arkade.sh
@@ -4,13 +4,53 @@ _ARKADE_INCLUDED_=1
 
 feature_arkade() {
 	FEAT_NAME="arkade"
-	FEAT_LIST_SCHEMA="0_11_40@x64:binary"
+	FEAT_LIST_SCHEMA="0_11_50@x64:binary"
 	
 	FEAT_DEFAULT_FLAVOUR="binary"
 
 	FEAT_DESC="arkade provides a portable marketplace for downloading your favourite devops CLIs and installing helm charts, with a single command"
 	FEAT_LINK="https://github.com/alexellis/arkade https://www.youtube.com/watch?v=8wU9s_mua8M"
 }
+
+feature_arkade_0_11_50() {
+
+	FEAT_VERSION="0_11_50"
+
+	if [ "$STELLA_CURRENT_PLATFORM" = "darwin" ]; then
+		if [ "$STELLA_CURRENT_CPU_FAMILY" = "intel" ]; then
+			FEAT_BINARY_URL_x64="https://github.com/alexellis/arkade/releases/download/0.11.50/arkade-darwin"
+			FEAT_BINARY_URL_FILENAME_x64="arkade-darwin-${FEAT_VERSION}"
+			FEAT_BINARY_URL_PROTOCOL_x64="HTTP"
+		fi
+		if [ "$STELLA_CURRENT_CPU_FAMILY" = "arm" ]; then
+			FEAT_BINARY_URL_x64="https://github.com/alexellis/arkade/releases/download/0.11.50/arkade-darwin-arm64"
+			FEAT_BINARY_URL_FILENAME_x64="arkade-darwin-arm64-${FEAT_VERSION}"
+			FEAT_BINARY_URL_PROTOCOL_x64="HTTP"
+		fi
+	fi
+	if [ "$STELLA_CURRENT_PLATFORM" = "linux" ]; then
+		if [ "$STELLA_CURRENT_CPU_FAMILY" = "intel" ]; then
+			FEAT_BINARY_URL_x64="https://github.com/alexellis/arkade/releases/download/0.11.50/arkade"
+			FEAT_BINARY_URL_FILENAME_x64="arkade-linux64-${FEAT_VERSION}"
+			FEAT_BINARY_URL_PROTOCOL_x64="HTTP"
+		fi
+		if [ "$STELLA_CURRENT_CPU_FAMILY" = "arm" ]; then
+			FEAT_BINARY_URL_x64="https://github.com/alexellis/arkade/releases/download/0.11.50/arkade-arm64"
+			FEAT_BINARY_URL_FILENAME_x64="arkade-arm64-${FEAT_VERSION}"
+			FEAT_BINARY_URL_PROTOCOL_x64="HTTP"
+			FEAT_BINARY_URL_x86="https://github.com/alexellis/arkade/releases/download/0.11.50/arkade-armhf"
+			FEAT_BINARY_URL_FILENAME_x86="arkade-armhf-${FEAT_VERSION}"
+			FEAT_BINARY_URL_PROTOCOL_x86="HTTP"
+		fi
+	fi
+
+	FEAT_ENV_CALLBACK="feature_arkade_env"
+
+	FEAT_INSTALL_TEST="$FEAT_INSTALL_ROOT/arkade"
+	FEAT_SEARCH_PATH="$FEAT_INSTALL_ROOT"
+}
+
+
 
 
 feature_arkade_0_11_40() {
