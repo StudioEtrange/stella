0edbd118e69cb593aa4945e14ad2d13ac7fa98c2
StudioEtrange <nomorgan@gmail.com>
2026-03-02 21:40:17 +0100

    feat(patchelf): update versions and add binary support feat(distrobox): update versions feat(cliproxyapi): add feature

--- nix/pool/feature-recipe/feature_distrobox.sh
+++ nix/pool/feature-recipe/feature_distrobox.sh
@@ -4,12 +4,27 @@ _distrobox_INCLUDED_=1
 
 feature_distrobox() {
 	FEAT_NAME="distrobox"
-	FEAT_LIST_SCHEMA="1_8_1_2:binary"
+	FEAT_LIST_SCHEMA="1_8_2_4:binary 1_8_1_2:binary"
 	
 	FEAT_DEFAULT_FLAVOUR="binary"
 
 	FEAT_DESC="Use any linux distribution inside your terminal. Enable both backward and forward compatibility with software and freedom to use whatever distribution youre more comfortable with. Runs on podman, docker or lilipod"
-	FEAT_LINK="https://gitlab.com/89luca89/distrobox https://distrobox.it/"
+	FEAT_LINK="https://github.com/89luca89/distrobox https://gitlab.com/89luca89/distrobox https://distrobox.it/"
+}
+
+
+
+
+feature_distrobox_1_8_2_4() {
+	FEAT_VERSION="1_8_2_4"
+
+	FEAT_BINARY_URL="https://github.com/89luca89/distrobox/archive/refs/tags/1.8.2.4.tar.gz"
+	FEAT_BINARY_URL_FILENAME="distrobox_1.8.2.4.tar.gz"
+	FEAT_BINARY_URL_PROTOCOL="HTTP_ZIP"
+
+
+	FEAT_INSTALL_TEST="${FEAT_INSTALL_ROOT}/distrobox"
+	FEAT_SEARCH_PATH="${FEAT_INSTALL_ROOT}"
 }
 
 
@@ -23,7 +38,6 @@ feature_distrobox_1_8_1_2() {
 
 	FEAT_INSTALL_TEST="${FEAT_INSTALL_ROOT}/distrobox"
 	FEAT_SEARCH_PATH="${FEAT_INSTALL_ROOT}"
-
 }
 
 
