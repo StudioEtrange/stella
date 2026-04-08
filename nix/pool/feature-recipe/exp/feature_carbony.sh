7b7bea6a7cea8b49362cf017f54b334e274bdab2
StudioEtrange <nomorgan@gmail.com>
2026-04-08 03:21:53 +0200

    nix : review feature carbonyl

--- nix/pool/feature-recipe/exp/feature_carbony.sh
+++
@@ -1,3 +0,0 @@
-# TODO
-# https://github.com/fathyb/carbonyl
-# Chromium running inside your terminal
--- nix/pool/feature-recipe/exp/feature_carbonyl.sh
+++ nix/pool/feature-recipe/exp/feature_carbonyl.sh
@@ -7,6 +7,27 @@ _CARBONYL_INCLUDED_=1
 # libasound2: ALSA library for audio playback https://formulae.brew.sh/formula/libsoundio
 # libfontconfig1: we need the configuration this package generates
 
+# forks
+# fork                          ahead  behind  stars  last_commit_date      pushed_at             url
+# Qubitdyne/carbonyl            41     0       6      2025-09-30T07:06:30Z  2025-09-30T08:30:55Z  https://github.com/Qubitdyne/carbonyl
+#	Support for Sixel rendering engine
+# JeffCarpenter/carbonyl        40     0       0      2025-09-30T07:06:08Z  2025-11-11T07:05:57Z  https://github.com/JeffCarpenter/carbonyl
+# 	Merge a lot of commits of Qubitdyne/carbonyl 
+# gyoz-ai/terminal-chromium     29     0       2      2026-03-29T11:19:48Z  2026-03-29T11:19:50Z  https://github.com/gyoz-ai/terminal-chromium
+#	Fixes for CI
+# jmagly/carbonyl               17     0       1      2026-04-06T05:44:33Z  2026-04-06T05:49:36Z  https://github.com/jmagly/carbonyl
+#	self declared as active fork - include chromium M120
+# arielm-cloudbuzz/carbonyl     14     0       0      2023-04-16T14:21:16Z  2023-04-16T14:21:16Z  https://github.com/arielm-cloudbuzz/carbonyl
+#	work on docker image for CI
+# AlexanderMaxRanabel/carbonyl  10     0       2      2024-07-01T14:31:48Z  2024-07-01T14:31:48Z  https://github.com/AlexanderMaxRanabel/carbonyl
+#	only add documentation
+# diyism/carbonyl               10     45      0      2025-12-16T14:36:04Z  2025-12-16T14:36:05Z  https://github.com/diyism/carbonyl
+#	only add documentation
+# aaronbanse/carbonyl-ftty      7      0       1      2026-02-09T00:28:14Z  2026-02-09T00:28:22Z  https://github.com/aaronbanse/carbonyl-ftty
+#	add new dependency fidelity - https://github.com/aaronbanse/fidelitty - library for high performance rendering in the terminal using unicode characters and escape sequences
+
+
+
 feature_carbonyl() {
 	FEAT_NAME="carbonyl"
 	FEAT_LIST_SCHEMA="0_0_3@x64:binary"
